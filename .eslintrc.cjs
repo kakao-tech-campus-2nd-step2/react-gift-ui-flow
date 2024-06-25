9fed7d5f8e4082ea4112ee1e1b2813c15ca740d4
cla6shade <cla6shade@gmail.com>
2024-06-26 04:01:21 +0900

    chore: react-in-jsx-option 규칙 off로 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
