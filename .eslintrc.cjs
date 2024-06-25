95434ec39f7c3720607e6c0864c56624aeb6c433
cla6shade <cla6shade@gmail.com>
2024-06-25 16:58:58 +0900

    chore: eslint 설정
    airbnb javascript code convention을 따르도록 설정.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,9 +2,9 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
