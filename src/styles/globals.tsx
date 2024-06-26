14cd6259953fb97add1299dc8040e72cf045eca9
Hyoeunkh <lhe4528@knu.ac.kr>
2024-06-26 20:56:12 +0900

    design: emotion을 활용하여 global style 설정

---
+++ src/styles/globals.tsx
@@ -0,0 +1,16 @@
+import emotionReset from 'emotion-reset';
+import { Global, css } from '@emotion/react';
+
+export const GlobalStyles = () => (
+  <Global
+    styles={css`
+      ${emotionReset}
+
+      *, *::after, *::before {
+        box-sizing: border-box;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+      }
+    `}
+  />
+);
