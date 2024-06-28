1d42545113ae60ca7016b7c41a60023fa88467a6
cla6shade <cla6shade@gmail.com>
2024-06-28 16:37:16 +0900

    feat: add Button.tsx

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,6 +1,38 @@
-function Button() {
-  // eslint-disable-next-line jsx-a11y/control-has-associated-label,react/button-has-type
-  return <button />;
+import { Theme } from '@styles/theme';
+import ElementSize from '@styles/size';
+import { css } from '@emotion/react';
+import { ButtonHTMLAttributes } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  theme: Theme,
+  size: ElementSize,
+  text: string,
+}
+function Button({
+  theme, size, text, ...rest
+}: ButtonProps) {
+  return (
+    <button
+      css={css`
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        font-size: 16px;
+        cursor: pointer;
+        background-color: ${theme.buttonBg};
+        color: ${theme.buttonText};
+        width: ${typeof size === 'string' ? 'auto' : size.width};
+        height: ${typeof size === 'string' ? 'auto' : size.height};
+        &:hover {
+          background-color: ${theme.buttonHover};
+        }
+      `}
+      type="button"
+      {...rest}
+    >
+      {text}
+    </button>
+  );
 }
 
 export default Button;
