1e64edfce3819d434e0b200ed586fa549622402b
cla6shade <cla6shade@gmail.com>
2024-06-28 16:37:52 +0900

    test: 버튼 스토리 작성

--- src/components/atoms/button/Button.stories.ts
+++ src/components/atoms/button/Button.stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { Themes } from '@styles/theme';
 import Button from './Button';
 
 const meta: Meta<typeof Button> = {
@@ -8,8 +9,27 @@ const meta: Meta<typeof Button> = {
 export default meta;
 
 type Story = StoryObj<typeof Button>;
-export const ButtonStory: Story = {
+export const LightResponsive: Story = {
   args: {
-
+    theme: Themes.THEME_LIGHT,
+    size: 'responsive',
+    text: 'Light Responsive',
+  },
+};
+export const DarkResponsive: Story = {
+  args: {
+    theme: Themes.THEME_DARK,
+    size: 'responsive',
+    text: 'Dark Responsive',
+  },
+};
+export const DarkFixed: Story = {
+  args: {
+    theme: Themes.THEME_DARK,
+    size: {
+      width: '120px',
+      height: '100px',
+    },
+    text: 'Dark Fixed',
   },
 };
