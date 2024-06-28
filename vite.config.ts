189b125a204973fb4f4eca31e203f09b858b7ac6
cla6shade <cla6shade@gmail.com>
2024-06-28 16:11:05 +0900

    chore: ReactElement attribute로 css를 사용할 수 있도록 번들러 및 tsconfig 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react({
+    jsxImportSource: '@emotion/react'
+  }), tsconfigPaths()],
 })
