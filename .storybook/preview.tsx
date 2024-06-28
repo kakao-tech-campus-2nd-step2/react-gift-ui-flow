aa7db6c4d5bd585bd60cf2c558fea416c842b23b
cla6shade <cla6shade@gmail.com>
2024-06-28 17:08:05 +0900

    feat: storybook preview에 global style 반영

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react';
+import GlobalStyle from '../src/styles/GlobalStyle';
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +10,13 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => {
+    return <GlobalStyle>
+      <Story />
+    </GlobalStyle>
+    }
+  ]
 };
 
 export default preview;
