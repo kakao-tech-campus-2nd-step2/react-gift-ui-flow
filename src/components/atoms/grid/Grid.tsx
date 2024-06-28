26f7106cfcfda688fbe079aebc38647cb2b67913
cla6shade <cla6shade@gmail.com>
2024-06-28 18:54:18 +0900

    feat: add Grid.tsx

---
+++ src/components/atoms/grid/Grid.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+interface GridProps {
+  gap?: string;
+  columns?: number;
+  children: React.ReactNode;
+}
+
+function Grid({ gap, columns, children }: GridProps) {
+  return (
+    <div css={css`
+    display: grid;
+    gap: ${gap};
+    grid-template-columns: repeat(${columns}, 1fr);
+  `}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Grid;
