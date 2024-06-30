7d9b09e9288da8dffe9e89ed5f24808a1924594a
Hyoeunkh <lhe4528@knu.ac.kr>
2024-06-30 15:53:41 +0900

    fix: [1주차 리뷰] style의 단위 오류 수정 및 Pick 사용으로 타입 지정

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -9,8 +9,9 @@ export const Image = ({ radius, ratio, ...rest }: IImage) => {
   return <StyleImg radius={radius} ratio={ratio} {...rest} />;
 };
 
-const StyleImg = styled.img<IImage>`
-  width: 100px;
-  border-radius: ${(props) => (props.radius === 'circle' ? '50%' : props.radius)};
+const StyleImg = styled.img<Pick<IImage, 'radius' | 'ratio'>>`
+  border-radius: ${(props) => (props.radius === 'circle' ? '50%' : `${props.radius}px`)};
   aspect-ratio: ${(props) => (props.ratio === 'square' ? '1 / 1' : props.ratio)};
+  object-fit: cover;
+  object-position: center center;
 `;
