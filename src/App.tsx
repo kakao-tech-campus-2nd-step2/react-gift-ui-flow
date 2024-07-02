fe6b9c28a24a1aebad0c1b33b001b8d3881d428c
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-07-02 16:25:55 +0900

    feat: add FriendSelector section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import resetStyles from '@/assets/styles/resetStyles';
 import Header from '@components/common/Header';
 import Footer from '@components/common/Footer';
 import Inner from '@components/common/Layout/Inner';
+import Image from '@components/common/Image/Image';
+import friend from '@assets/images/friend.png';
+import styled from '@emotion/styled';
+import Container from '@components/common/Layout/Container/Container';
 
 function App() {
   return (
@@ -11,6 +15,16 @@ function App() {
       <Global styles={resetStyles} />
       <Inner maxWidth={1024}>
         <Header />
+      </Inner>
+      <FriendSelectorContainer>
+        <Inner maxWidth={1024}>
+          <Container alignItems="center">
+            <Image src={friend} width={70} height={70} radius={24} alt="친구 선택 유도 " />
+            <Description>선물 받을 친구를 선택해주세요.</Description>
+          </Container>
+        </Inner>
+      </FriendSelectorContainer>
+      <Inner maxWidth={1024}>
         <Footer />
       </Inner>
     </main>
@@ -18,3 +32,13 @@ function App() {
 }
 
 export default App;
+
+const FriendSelectorContainer = styled.section`
+  padding: 40px 0;
+  background-color: #fafafa;
+`;
+
+const Description = styled.p`
+  padding-left: 16px;
+  font-size: 28px;
+`;
