75ea935debc9f9e7545d3fa3844414cfd4aee164
cla6shade <cla6shade@gmail.com>
2024-07-02 15:48:55 +0900

    feat: PageFooter 추가

---
+++ src/components/organisms/PageFooter.tsx
@@ -0,0 +1,19 @@
+import Container from '@components/atoms/container/Container';
+import { css } from '@emotion/react';
+import { backgroundColors } from '@styles/colors';
+
+function PageFooter() {
+  return (
+    <footer css={css`
+        padding: 28px 16px 88px;
+        background-color: ${backgroundColors.root}
+    `}
+    >
+      <Container backgroundColor={backgroundColors.root}>
+        <p>카카오톡 선물하기</p>
+      </Container>
+    </footer>
+  );
+}
+
+export default PageFooter;
