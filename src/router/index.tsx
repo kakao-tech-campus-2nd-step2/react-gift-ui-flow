25908d11abe2626f038c85c6a74739ca494e2088
cla6shade <cla6shade@gmail.com>
2024-07-02 22:43:43 +0900

    feat: 메인 페이지 라우팅 규칙 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
+import MainPage from '@pages/main/MainPage';
+import Paths from '@constants/Paths';
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: Paths.MAIN_PAGE,
+    element: <MainPage />,
+  },
+]);
 
 export default router;
