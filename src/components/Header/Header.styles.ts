438b878010ae6464102176c14cd11fd955b92bd9
KimJi-An <j0012a@naver.com>
2024-07-02 16:45:43 +0900

    design: Header 컴포넌트의 Inner에 max-width 설정

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -9,9 +9,8 @@ export const Inner = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  /* margin: 0 auto; */
   padding: 10px 20px;
-  /* max-width: 1200px; */
+  max-width: 1200px;
 `;
 
 export const Logo = styled.a`
@@ -27,7 +26,7 @@ export const Login = styled.a`
   text-decoration: none;
   color: #000;
   cursor: pointer;
-  
+
   &:hover {
     text-decoration: underline;
   }
