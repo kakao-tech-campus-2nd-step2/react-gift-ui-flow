2d0cde75499d4fef280ac3702e901d258eb99cc0
cla6shade <cla6shade@gmail.com>
2024-07-02 16:47:15 +0900

    feat: Container가 기타 css property를 받아 적용할 수 있도록 변경

--- src/components/atoms/container/Container.tsx
+++ src/components/atoms/container/Container.tsx
@@ -1,5 +1,5 @@
-import { ReactNode } from 'react';
-import { css } from '@emotion/react';
+import { CSSProperties, ReactNode } from 'react';
+import { css, CSSObject } from '@emotion/react';
 import { backgroundColors } from '@styles/colors';
 import { FixedSize } from '@styles/size';
 
@@ -11,23 +11,27 @@ interface ContainerProps {
   alignItems?: 'center' | 'flex-start' | 'flex-end' | 'baseline' | 'stretch',
   children?: ReactNode,
   backgroundColor?: string;
+  cssProps?: CSSProperties;
 }
 
 function Container({
-  maxWidth, elementSize, flexDirection, justifyContent, alignItems, children, backgroundColor,
+  maxWidth, elementSize, flexDirection, justifyContent, alignItems, children, backgroundColor, cssProps,
 }: ContainerProps) {
+  const combinedStyles: CSSObject = {
+    maxWidth,
+    display: 'flex',
+    width: elementSize ? elementSize.width : 'auto',
+    height: elementSize ? elementSize.height : 'auto',
+    flexDirection,
+    justifyContent,
+    alignItems,
+    backgroundColor: backgroundColor ?? backgroundColors.container,
+    ...cssProps,
+  };
+
   return (
     <div
-      css={css`
-        max-width: ${maxWidth};
-        display: flex;
-        width: ${elementSize ? elementSize.width : 'auto'};
-        height: ${elementSize ? elementSize.height : 'auto'};
-        flex-direction: ${flexDirection};
-        justify-content: ${justifyContent};
-        align-items: ${alignItems};
-        background-color: ${backgroundColor ?? backgroundColors.container};
-      `}
+      css={css(combinedStyles)}
     >
       {children}
     </div>
