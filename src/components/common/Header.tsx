0144a3590736f435422c37ab7cce4d3d4eee0158
tteokbokki-master <0jin.git@gmail.com>
2024-07-02 12:48:07 +0900

    Feat: Header 컴포넌트 구현

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+
+const StyledHeader = styled.header`
+  position: fixed;
+  z-index: 9999;
+  width: 100%;
+  max-width: 100vw;
+  height: 54px;
+  background-color: rgb(255, 255, 255);
+  padding: 0px 16px;
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledInner = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    height: 54px;
+  }
+`;
+
+const StyledLinkText = styled(Link)`
+  align-items: center;
+  font-size: 14px;
+  color: rgb(0, 0, 0);
+  text-decoration: none;
+  cursor: pointer;
+`;
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <StyledInner>
+        <Link to="/">
+          <img
+            src="https://gift-s.kakaocdn.net/dn/gift/images/m640/pc_gift_logo.png"
+            alt="카카오 선물하기 로고"
+          />
+        </Link>
+        <StyledLinkText to="/my-account">내 계정</StyledLinkText>
+      </StyledInner>
+    </StyledHeader>
+  );
+};
+
+export default Header;
