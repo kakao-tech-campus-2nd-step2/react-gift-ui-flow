6ec9f99649089c8ac9cafb5f0808b965334564af
tteokbokki-master <0jin.git@gmail.com>
2024-07-02 22:21:50 +0900

    Feat: 나의 페이지 구현

--- src/pages/MyAccount.tsx
+++ src/pages/MyAccount.tsx
@@ -1,8 +1,47 @@
+import styled from '@emotion/styled';
+
+const MyAccountContainer = styled.div`
+  width: 100%;
+  padding: 80px 0px 120px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-weight: 700;
+  font-size: 36px;
+`;
+
+const DivHeight = styled.div`
+  width: 100%;
+  height: 64px;
+`;
+
+const StyledButton = styled.button`
+  max-width: 200px;
+  width: 100%;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: background-color 200ms ease 0s;
+  height: 40px;
+  font-size: 15px;
+  color: rgb(255, 255, 255);
+  background-color: rgb(68, 68, 68);
+
+  &:hover {
+    background-color: rgb(85, 85, 85);
+  }
+`;
+
 const MyAccount = () => {
   return (
-    <div>
-      <h1>MyAccount</h1>
-    </div>
+    <MyAccountContainer>
+      {`${''}님 안녕하세요!`}
+      <DivHeight />
+      <StyledButton>로그아웃</StyledButton>
+    </MyAccountContainer>
   );
 };
 
