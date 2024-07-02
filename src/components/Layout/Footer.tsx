2ff320933e62e4a252eea59774481c5be2fd95a8
minji2219 <200146@jnu.ac.kr>
2024-07-02 14:32:00 +0900

    Feat: Footer 생성

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Footer = () => {
+  return <Wrapper>카카오톡 선물하기</Wrapper>;
+};
+const Wrapper = styled.div`
+  background-color: rgb(250, 250, 250);
+  height: 76px;
+  padding: 20px;
+`;
