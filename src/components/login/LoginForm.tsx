b929af844f574a5129bf4d1716fee665c7eb12a9
tteokbokki-master <0jin.git@gmail.com>
2024-07-02 22:11:57 +0900

    Feat: 로그인 페이지 구현

---
+++ src/components/login/LoginForm.tsx
@@ -0,0 +1,118 @@
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LoginFormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  img {
+    width: 88px;
+    color: rgb(51, 51, 51);
+  }
+`;
+
+const StyledLoginForm = styled.article`
+  border: 1px solid rgba(0, 0, 0, 0.12);
+  padding: 60px 52px;
+  width: 100%;
+  max-width: 580px;
+  box-sizing: border-box;
+
+  & > div:nth-of-type(1) {
+    width: 100%;
+    height: 16px;
+  }
+  & > div:nth-of-type(2) {
+    width: 100%;
+    height: 60px;
+  }
+`;
+
+const StyledInput = styled.input`
+  min-height: 46px;
+  font-size: 18px;
+  line-height: 25px;
+  padding: 10px 0px 8px;
+  border-width: 0px 0px 2px;
+  width: 100%;
+  box-sizing: border-box;
+  color: rgb(25, 25, 25);
+  transition: border-color 200ms ease 0s;
+  border-style: solid;
+  border-color: rgb(204, 204, 204);
+  outline: none;
+
+  &:focus {
+    border-color: rgb(37, 37, 37);
+  }
+`;
+
+const StyledButton = styled.button`
+  height: 60px;
+  font-size: 16px;
+  width: 100%;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  transition: background-color 200ms ease 0s;
+  color: rgb(17, 17, 17);
+  background-color: rgb(254, 229, 0);
+
+  &:hover {
+    background-color: rgb(250, 218, 10);
+  }
+`;
+
+const LoginForm = () => {
+  const nav = useNavigate();
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    password: '',
+  });
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUserInfo({
+      ...userInfo,
+      [name]: value,
+    });
+  };
+
+  const userLogin = () => {
+    if (userInfo.name === '' || userInfo.password === '') {
+      alert('아이디와 비밀번호를 입력해주세요.');
+    } else {
+      nav('/');
+    }
+  };
+
+  return (
+    <LoginFormContainer>
+      <img
+        src="https://kakao-tech-week2-kakao-gift-ui-flow.pages.dev/static/media/kakao_logo.60e99555125ae4c4b8f23f6c5faee9ec.svg"
+        alt="카카오 CI"
+      />
+      <StyledLoginForm>
+        <StyledInput placeholder="이름" value={userInfo.name} name="name" onChange={handleInput} />
+        <div />
+        <StyledInput
+          type="password"
+          name="password"
+          placeholder="비밀번호"
+          value={userInfo.password}
+          onChange={handleInput}
+        />
+        <div />
+        <StyledButton onClick={userLogin}>로그인</StyledButton>
+      </StyledLoginForm>
+    </LoginFormContainer>
+  );
+};
+
+export default LoginForm;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,20 @@
+import styled from '@emotion/styled';
+
+import LoginForm from '@/components/login/LoginForm';
+
+const LoginContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <LoginContainer>
+      <LoginForm />
+    </LoginContainer>
   );
 };
 
