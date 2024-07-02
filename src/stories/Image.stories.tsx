fba659b285f426d3cf24779f13815455ddcaa376
KimJi-An <j0012a@naver.com>
2024-07-02 21:51:29 +0900

    refactor: Image 스토리북의 argTypes 제거

--- src/stories/Image.stories.tsx
+++ src/stories/Image.stories.tsx
@@ -8,64 +8,36 @@ const meta: Meta<typeof Image> = {
   parameters: {
     layout: 'centered',
   },
-  argTypes: {
-    src: { control: 'text' },
-    alt: { control: 'text' },
-    ratio: {
-      control: {
-        type: 'select',
-        options: ['square', 16 / 9, 4 / 3, 1, 3 / 2, 2 / 3, 9 / 16],
-      },
-    },
-    radius: {
-      control: {
-        type: 'select',
-        options: ['circle', 0, 4, 8, 16, 32, 64],
-      },
-    },
-  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof Image>;
 
-export const Default: Story = {
+export const RatioSquare: Story = {
   render: (args) => <Image {...args} />,
   args: {
     src: 'https://t1.kakaocdn.net/friends/www/talk/kakaofriends_talk_2018.png',
-    alt: 'Placeholder Image',
-    ratio: 1,
-    radius: 0,
-  },
-};
-
-export const Square: Story = {
-  render: (args) => <Image {...args} />,
-  args: {
-    src: 'https://t1.kakaocdn.net/friends/www/talk/kakaofriends_talk_2018.png',
-    alt: 'Square Image',
+    width: '200px',
     ratio: 'square',
     radius: 0,
   },
 };
 
-export const Circle: Story = {
+export const RadiusCircle: Story = {
   render: (args) => <Image {...args} />,
   args: {
     src: 'https://t1.kakaocdn.net/friends/www/talk/kakaofriends_talk_2018.png',
-    alt: 'Circle Image',
-    ratio: 1,
+    width: '200px',
+    ratio: 'square',
     radius: 'circle',
   },
 };
 
-export const Rounded: Story = {
+export const RadiusRound: Story = {
   render: (args) => <Image {...args} />,
   args: {
     src: 'https://t1.kakaocdn.net/friends/www/talk/kakaofriends_talk_2018.png',
-    alt: 'Rounded Image',
-    ratio: 1,
     radius: 10,
   },
 };
