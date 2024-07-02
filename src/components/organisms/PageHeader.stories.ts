b38e2ecf96dac2e35624c70cf959302418c884bd
cla6shade <cla6shade@gmail.com>
2024-07-02 15:39:00 +0900

    test: PageHeader 스토리 작성

---
+++ src/components/organisms/PageHeader.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PageHeader from './PageHeader';
+
+const meta: Meta<typeof PageHeader> = {
+  component: PageHeader,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof PageHeader>;
+
+export const DefaultHeader: Story = {
+  args: {
+    displayLogo: true,
+    isLoggedIn: false,
+  },
+};
