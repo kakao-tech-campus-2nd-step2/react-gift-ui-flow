59ece9f20db20e46170c5ceb8dd877d9ead03de4
cla6shade <cla6shade@gmail.com>
2024-07-02 15:38:51 +0900

    feat: PageHeader 추가

---
+++ src/components/organisms/PageHeader.tsx
@@ -0,0 +1,46 @@
+import Container from '@components/atoms/container/Container';
+import { css } from '@emotion/react';
+import headerLogo from '@assets/images/header_logo.png';
+import styled from '@emotion/styled';
+
+interface PageHeaderProps {
+  isLoggedIn?: boolean;
+  displayLogo?: boolean;
+}
+
+const HeaderAccountButton = styled.p`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+`;
+
+function PageHeader({ isLoggedIn, displayLogo }: PageHeaderProps) {
+  return (
+    <header css={css`
+        position: fixed;
+        left: 0;
+        right: 0;
+        padding: 0 16px;
+        z-index: 9999;
+    `}
+    >
+      <Container elementSize={{ height: '54px', width: '100%' }} justifyContent="space-between">
+        {displayLogo
+          ? (
+            <img
+              css={css`
+                  height: 100%;
+              `}
+              src={headerLogo}
+            />
+          ) : null}
+        <HeaderAccountButton>
+          {/* TODO: 로그인 페이지 또는 마이페이지로 이동하게끔 연결하기 */}
+          {isLoggedIn ? '마이페이지' : '로그인'}
+        </HeaderAccountButton>
+      </Container>
+    </header>
+  );
+}
+
+export default PageHeader;
