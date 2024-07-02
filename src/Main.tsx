7885815eb864397a0f36aa2bca7d23a903a1721a
Solbin Jung <jsolbin0724@gmail.com>
2024-07-03 01:44:55 +0900

    feat: Image 컴포넌트 이용해 themes에 이미지 추가

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,22 +2,23 @@ import styled from '@emotion/styled';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { Grid } from './components/common/layouts/Grid';
-
+import { Image } from '@/components/common/Image';
+import { Grid } from '@/components/common/layouts/Grid';
+ 
 const Main: React.FC = () => {
   const themes = [
-    { key: 'theme1', name: '생일' },
-    { key: 'theme2', name: '졸업선물' },
-    { key: 'theme3', name: '스몰럭셔리' },
-    { key: 'theme4', name: '명품선물' },
-    { key: 'theme5', name: '결혼/집들이' },
-    { key: 'theme6', name: '따뜻한선물' },
-    { key: 'theme7', name: '가벼운선물' },
-    { key: 'theme8', name: '팬심저격' },
-    { key: 'theme9', name: '교환권' },
-    { key: 'theme10', name: '건강/비타민' },
-    { key: 'theme11', name: '과일/한우' },
-    { key: 'theme12', name: '출산/키즈' },
+    { key: 'theme1', name: '생일', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
+    { key: 'theme2', name: '졸업선물', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
+    { key: 'theme3', name: '스몰럭셔리', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
+    { key: 'theme4', name: '명품선물', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
+    { key: 'theme5', name: '결혼/집들이', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
+    { key: 'theme6', name: '따뜻한선물', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
+    { key: 'theme7', name: '가벼운선물', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
+    { key: 'theme8', name: '팬심저격', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
+    { key: 'theme9', name: '교환권', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
+    { key: 'theme10', name: '건강/비타민', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
+    { key: 'theme11', name: '과일/한우', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
+    { key: 'theme12', name: '출산/키즈', imgSrc: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png' },
   ];
 
 const MainContainer = styled.main`
@@ -43,6 +44,16 @@ const ThemeLink = styled(Link)`
   }
 `;
 
+const ThemeItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const ImageContainer = styled.div`
+    margin-bottom: 3px; 
+`;
+
   return (
     <div>
       <MainContainer>
@@ -51,7 +62,12 @@ const ThemeLink = styled(Link)`
       <Grid columns={4} gap={20}>
         {themes.map((theme) => (
           <ThemeLink key={theme.key} to={`/theme/${theme.key}`}>
-            {theme.name}
+            <ThemeItem>
+              <ImageContainer>
+                <Image src={theme.imgSrc} width="60px" ratio="square" radius={20} />
+              </ImageContainer>
+              <span>{theme.name}</span>
+            </ThemeItem>
           </ThemeLink>
         ))}
       </Grid>
