d16c4397b5e6e7ea5aec487067ae9c629ea32546
minji2219 <200146@jnu.ac.kr>
2024-07-02 15:06:57 +0900

    Feat: 메인페이지 메인 멘트 컴포넌트 구현

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,14 @@
+import styled from '@emotion/styled';
+
+import { MainMent } from './MainMent';
+
 export const Home = () => {
-  return <div>메인 페이지</div>;
+  return (
+    <Wrapper>
+      <MainMent />
+    </Wrapper>
+  );
 };
+const Wrapper = styled.div`
+  width: 100%;
+`;
