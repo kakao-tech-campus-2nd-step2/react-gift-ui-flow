1f979599d8566aa03037f82a18d23c09e411991f
KimJi-An <j0012a@naver.com>
2024-07-02 15:30:54 +0900

    feat: Footer 컴포넌트 구현

---
+++ src/components/Footer/Footer.styles.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.footer`
+  width: 100%;
+  background-color: #fafafc;
+  padding: 20px 0;
+`;
+
+export const Text = styled.p`
+  font-size: 14px;
+  color: #000;
+`;
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Text } from '@components/Footer/Footer.styles';
+
+const Footer = () => (
+  <Container>
+    <Text>카카오톡 선물하기</Text>
+  </Container>
+);
+
+export default Footer;
