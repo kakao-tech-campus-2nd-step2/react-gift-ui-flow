62a13d13055856326e6b067f8c62391fbcf8a3b1
cla6shade <cla6shade@gmail.com>
2024-07-03 02:03:59 +0900

    feat: ThemeItem 추가

---
+++ src/components/molecules/themeItem/ThemeItem.tsx
@@ -0,0 +1,31 @@
+import GiftThemes, { GiftThemeKey } from '@constants/GiftThemes';
+// import Paths from '@constants/Paths';
+import Container from '@components/atoms/container/Container';
+import Image from '@components/atoms/image/Image';
+import { css } from '@emotion/react';
+
+interface ThemeItemProps {
+  themeKey: GiftThemeKey;
+}
+
+function ThemeItem({ themeKey }: ThemeItemProps) {
+  // const href = Paths.THEME_PAGE(themeKey);
+  const theme = GiftThemes[themeKey];
+
+  return (
+    <Container cssProps={{ padding: '25px 35px 24px' }}>
+      <Container elementSize={{ width: '100%', height: 'auto' }} flexDirection="column" alignItems="center">
+        <Image src={theme.themeImageSrc} ratio="square" radius={32} />
+        <p css={css`
+          font-size: 16px;
+          padding-top: 7px;
+        `}
+        >
+          {theme.keyword}
+        </p>
+      </Container>
+    </Container>
+  );
+}
+
+export default ThemeItem;
