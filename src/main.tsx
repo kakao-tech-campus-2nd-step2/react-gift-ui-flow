b55cc6d6f5732c87d037dc5e54e6cdf8f6eb5ff6
cla6shade <cla6shade@gmail.com>
2024-07-02 22:22:13 +0900

    design: global reset.css 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/App';
+import router from '@router';
+import { RouterProvider } from 'react-router-dom';
+import GlobalStyle from '@styles/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
