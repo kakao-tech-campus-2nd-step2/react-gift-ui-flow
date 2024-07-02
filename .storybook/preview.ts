c02507255d8d74ff88ab87932901b97ab55ef846
unknown <kc21ynje@gmail.com>
2024-07-02 15:03:09 +0900

    chore: set eslint & prettier

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,9 @@
-import type { Preview } from '@storybook/react';
-import '@/styles';
+import type { Preview } from "@storybook/react";
+import "@/styles";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
--- src/components/common/layouts/Grid/index.stories.tsx
+++ src/components/common/layouts/Grid/index.stories.tsx
@@ -1,6 +1,6 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from "@storybook/react";
 
-import { Grid } from '.';
+import { Grid } from ".";
 
 const ChildComponent = () => (
   <>
@@ -25,10 +25,10 @@ const ChildComponent = () => (
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Common/Layout/Grid',
+  title: "Common/Layout/Grid",
   component: Grid,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
 } satisfies Meta<typeof Grid>;
 
 export default meta;
