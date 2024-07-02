460ecd5c2e38d8087fd22b03fe13f66e12f48266
KimJi-An <j0012a@naver.com>
2024-07-03 00:33:03 +0900

    refactor: Container 스토리북에서 불필요한 부분 삭제

--- src/stories/Container.stories.tsx
+++ src/stories/Container.stories.tsx
@@ -5,31 +5,6 @@ import Container from '../components/Layout/Container';
 const meta: Meta<typeof Container> = {
   title: 'Components/Layout/Container',
   component: Container,
-  parameters: {
-    layout: 'centered',
-  },
-  argTypes: {
-    maxWidth: { control: 'text' },
-    flexDirection: {
-      control: 'select',
-      options: ['row', 'column'],
-    },
-    justifyContent: {
-      control: 'select',
-      options: [
-        'center',
-        'flex-start',
-        'flex-end',
-        'space-between',
-        'space-around',
-      ],
-    },
-    alignItems: {
-      control: 'select',
-      options: ['center', 'flex-start', 'flex-end', 'baseline', 'stretch'],
-    },
-    children: { control: 'text' },
-  },
 };
 
 export default meta;
@@ -39,9 +14,9 @@ type Story = StoryObj<typeof Container>;
 export const Default: Story = {
   render: (args) => <Container {...args} />,
   args: {
-    maxWidth: '800px',
+    maxWidth: '1200px',
     flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     children: 'Hello, world!',
   },
