01494cd01fd4952ad36d2fd66836b866622094da
JeongSeoyun <xeonj@naver.com>
2024-07-03 18:19:12 +0900

    style: Header padding 값 화면 너비에 따라 변경

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,20 +1,34 @@
 import styled from '@emotion/styled';
 
 export type HeaderProps = {
-    title: string;
-  }
+  title: string;
+  subtitle: string;
+}
 
 const HeaderContainer = styled.header`
   background-color: #fff;
   color: #333;
-  padding: 20px 20px;
+  padding: 20px calc(8% + 20px);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   font-size: 10px;
   font-weight: bold;
+
+  @media (max-width: 992px) {
+    padding: 20px calc(4% + 20px);
+  }
+
+  @media (max-width: 768px) {
+    padding: 20px 20px;
+  }
+  
 `;
 
-const Header:React.FC<HeaderProps> = ({ title }) => (
+const Header: React.FC<HeaderProps> = ({ title, subtitle }) => (
   <HeaderContainer>
     <h1>{title}</h1>
+    <h2>{subtitle}</h2>
   </HeaderContainer>
 );
 
