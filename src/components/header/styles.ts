e2080e7b7e00934d1b306f830ae24775752509f1
Dobbymin <gmin0701@knu.ac.kr>
2024-07-03 14:43:52 +0900

    feat(header): css 코드 구현

---
+++ src/components/header/styles.ts
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+export const HeaderLayout = styled.header`
+  position: fixed;
+  z-index: 9999;
+  padding: 0 1rem;
+  width: 100%;
+  max-width: 100vw;
+  height: 54px;
+  background-color: #fff;
+`;
+
+export const GiftLogo = styled.img`
+  height: 54px;
+  cursor: pointer;
+`;
+
+export const Login = styled.p`
+  font-size: 14px;
+  color: #000;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+export const InnerContainer = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
