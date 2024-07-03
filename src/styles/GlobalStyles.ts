84d77fd93b7ada1aff977526fcbc8f9cf10ab686
Dobbymin <gmin0701@knu.ac.kr>
2024-07-03 15:10:06 +0900

    feat(GlobalStyles): 전역 css 코드 구현

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+export const GlobalStyle = css`
+  * {
+    box-sizing: border-box;
+  }
+
+  img {
+    display: block;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+`;
