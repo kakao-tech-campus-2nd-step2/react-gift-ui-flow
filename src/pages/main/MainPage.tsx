38d7c222e09d5c40fa49d2729ee0b1ded68878ad
cla6shade <cla6shade@gmail.com>
2024-07-03 15:08:02 +0900

    refactor: 재사용성이 낮은 메인배너를 main/ 카테고리화

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,6 +1,6 @@
 import Page from '@components/templates/Page';
 import Container from '@components/atoms/container/Container';
-import MainBanner from '@components/organisms/banner/MainBanner';
+import MainBanner from '@components/organisms/main/MainBanner';
 import ThemeSection from '@components/organisms/main/ThemeSection';
 import AIButtonSection from '@components/organisms/main/AIButtonSection';
 import RankingSection from '@components/organisms/main/ranking/RankingSection';
