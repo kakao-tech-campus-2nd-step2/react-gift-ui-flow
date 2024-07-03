598be919aae89fa6684088ff58dc467a492adbcf
mingkyeongg <ming0820@naver.com>
2024-07-03 14:04:20 +0900

    feat: add category section at main page

---
+++ src/components/common/Items/items.tsx
@@ -0,0 +1,35 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import React from 'react';
+
+import { Image } from '@/components/common/Image/index';
+
+const itemStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+  span {
+    margin-top: 10px;
+    font-size: 14px;
+  }
+`;
+
+interface ItemProps {
+  image: string;
+  label: string;
+  radius?: 'circle' | number;
+}
+
+const Item: React.FC<ItemProps> = ({ image, label, radius}) => {
+	radius = 20;
+  return (
+    <div css={itemStyle}>
+      <Image src={image} alt={label} radius={radius} ratio="square" css={{ width: 60, height: 60 }}/>
+      <span>{label}</span>
+    </div>
+  );
+};
+
+export default Item;
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,44 @@
-const Home = () => {
-  return (
-	<div>
-	  <h1>Home</h1>
-	</div>
-  );
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+
+import Item from '../components/common/Items/items';
+import { Grid }  from '../components/common/layouts/Grid/index';
+
+interface ItemType {
+  image: string;
+  label: string;
+  radius?: 'circle' | number;
+}
+
+const imageUrls = {
+	img: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png'
 };
+
+const items: ItemType[] = [
+  { image: imageUrls.img, label: '생일'},
+  { image: imageUrls.img, label: '졸업선물'},
+  { image: imageUrls.img, label: '명품선물'},
+  { image: imageUrls.img, label: '스몰럭셔리'},
+  { image: imageUrls.img, label: '결혼/집들이' },
+  { image: imageUrls.img, label: '따뜻한선물' },
+  { image: imageUrls.img, label: '가벼운선물' },
+  { image: imageUrls.img, label: '팬심저격' },
+  { image: imageUrls.img, label: '교환권' },
+  { image: imageUrls.img, label: '건강/비타민' },
+  { image: imageUrls.img, label: '과일/한우' },
+  { image: imageUrls.img, label: '출산/키즈' },
+];
+
+const Home: React.FC = () => {
+	return (
+	  <div>
+		<Grid columns={4} gap={20}>
+		  {items.map((item, index) => (
+			<Item key={index} image={item.image} label={item.label} radius={item.radius} />
+		  ))}
+		</Grid>
+	  </div>
+	);
+  };
 
 export default Home;
