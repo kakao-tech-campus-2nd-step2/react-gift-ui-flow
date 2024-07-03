1076afa42e7617572e3ff97d52e5face16567b5d
dandamdandam <glue0440@gmail.com>
2024-07-03 15:30:44 +0900

    feat: add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import styled from '@emotion/styled';
+import Footer from "./components/Footer";
 
 const App = () => {
-    const name = 'Josh Perez';
 
     return (
         <div>
-            <Title>Hello, {name}</Title>
+            <Footer/>
         </div>
     );
 };
 
 export default App;
-
-const Title = styled.h1`
-    font-size: 1.5em;
-    color: gray;
-`;
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,22 @@
+import styled from "@emotion/styled";
+
+import { Grid } from "../common/layouts/Grid";
+
 export default () => {
-    return <footer></footer>;
+    return (
+        <Footar>
+            <Grid columns={2}>
+                <div>카카오톡 선물하기</div>
+            </Grid>
+        </Footar>
+    );
 };
 
-const Footar = styled.footer();
+const Footar = styled.footer(
+    {
+        padding: '28px 16px 88px',
+        width: '100%',
+        maxWidth: '100vw',
+        backgroundColor: 'rgb(250, 250, 252)',
+    }
+);
