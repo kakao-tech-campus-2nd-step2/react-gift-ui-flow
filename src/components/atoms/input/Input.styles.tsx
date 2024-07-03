b8e93fe575857debd8a13a4ab9d11b2470200baa
cla6shade <cla6shade@gmail.com>
2024-07-03 16:14:06 +0900

    design: StyledInput 스타일 조정

--- src/components/atoms/input/Input.styles.tsx
+++ src/components/atoms/input/Input.styles.tsx
@@ -6,13 +6,14 @@ export const StyledInput = styled.input<InputProps>`
   width: ${({ elementSize }) => (typeof elementSize === 'string' ? 'auto' : elementSize.width)};
   height: ${({ elementSize }) => (typeof elementSize === 'string' ? 'auto' : elementSize.height)};
   border: none;
-  border-bottom: #939393 3px solid;
+  border-bottom: #939393 2px solid;
   padding-left: 7px;
   transition: all 0.2s ease-in;
+  font-size: 16px;
 
   &:focus {
     outline: none;
-    border-bottom: #494949 3px solid;
+    border-bottom: #494949 2px solid;
   }
 `;
 
