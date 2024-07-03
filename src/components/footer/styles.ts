89d6aa502994577b5e135949f7df61048d78b0ed
Dobbymin <gmin0701@knu.ac.kr>
2024-07-03 14:29:24 +0900

    feat(footer): css 구현

---
+++ src/components/footer/styles.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const FooterLayout = styled.footer`
+  width: 100%;
+  padding: 28px 15px 88px;
+  max-width: 100vw;
+  bottom: 0;
+  background-color: rgb(250, 250, 252);
+`;
