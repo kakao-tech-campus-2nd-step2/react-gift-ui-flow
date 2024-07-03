c4f09533183470fe8670c49cb7c53ce9d0abd7cd
gimdogyun <jasper12394@gmail.com>
2024-07-03 15:39:02 +0900

    feat : setting router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,9 +1,19 @@
+import MainLayout from '@layouts/MainLayout';
+import Home from '@pages/Home';
+import Login from '@pages/Login';
+import MyAccount from '@pages/MyAccount';
+import Theme from '@pages/Theme';
 import { Route, Routes } from 'react-router-dom';
 
 function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<div />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/theme/:themeKey" element={<Theme />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/my-account" element={<MyAccount />} />
+      </Route>
     </Routes>
   );
 }
