c7ea89899c56c6c281f737ba9eefe7bef684dc82
Dobbymin <gmin0701@knu.ac.kr>
2024-07-03 14:46:11 +0900

    Feat(App.tsx): router 기능 추가 및 코드 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import styled from '@emotion/styled';
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './router';
 
 const App = () => {
-  const name = 'Josh Perez';
-
-  return (
-    <div>
-      <Title>Hello, {name}</Title>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  color: gray;
-`;
