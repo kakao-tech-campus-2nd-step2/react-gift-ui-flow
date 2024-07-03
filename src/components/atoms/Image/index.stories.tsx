2c9acf132287b8d9a07e58ef47da1153a63069b7
JaeAnHan <jaean1999@naver.com>
2024-07-03 15:36:50 +0900

    chore: atomic design 도입

--- src/components/atoms/Image/index.stories.tsx
+++ src/components/atoms/Image/index.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Image } from '.';
+import { Image } from './index';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
