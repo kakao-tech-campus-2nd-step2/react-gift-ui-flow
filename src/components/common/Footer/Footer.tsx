217e16406eaf2a31c6a23dd0fb8e929b8cb9037b
mingkyeongg <ming0820@naver.com>
2024-07-03 14:46:00 +0900

    design: add footer style

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,10 +1,20 @@
+import styled from '@emotion/styled';
 import React from "react";
+
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  padding: 30px 20px;
+  width: 100%;
+  height: 150px;
+  background-color: #f5f5f5;
+`;
 
 const Footer: React.FC = () => {
 	return (
-	<footer>
-	  <p>Footer</p>
-	</footer>
+	<FooterContainer>
+	  <p>카카오톡 선물하기</p>
+	</FooterContainer>
   );
 }
 
