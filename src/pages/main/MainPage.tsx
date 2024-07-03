b6b82251442b8abc03b39f8d4161fa50843bf12d
cla6shade <cla6shade@gmail.com>
2024-07-03 14:53:09 +0900

    feat: 컴포넌트 조합하여 메인 페이지 작성

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,9 +1,20 @@
 import Page from '@components/templates/Page';
+import Container from '@components/atoms/container/Container';
+import MainBanner from '@components/organisms/banner/MainBanner';
+import ThemeSection from '@components/organisms/main/ThemeSection';
+import AIButtonSection from '@components/organisms/main/AIButtonSection';
+import RankingSection from '@components/organisms/main/ranking/RankingSection';
 
 function MainPage() {
   return (
     <Page>
-      {/* content */}
+      <Container flexDirection="column" elementSize="full-width">
+        <MainBanner />
+        <ThemeSection maxColumns={6} />
+        <AIButtonSection />
+        <Container elementSize={{ width: '100%', height: '120px' }} />
+        <RankingSection />
+      </Container>
     </Page>
   );
 }
