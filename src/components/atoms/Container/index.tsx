6d4ce5359aedfe1a4dfbc0d4bb76486c3214f1eb
JaeAnHan <jaean1999@naver.com>
2024-07-03 20:00:19 +0900

    feat: flex 관련 속성 props로 받도록 변경

--- src/components/atoms/Container/index.tsx
+++ src/components/atoms/Container/index.tsx
@@ -5,13 +5,32 @@ import { vars } from '@/styles';
 
 type Props = {
   maxWidth?: string;
+  flexDirection?: string;
+  alignItems?: string;
+  justifyContent?: string;
 } & React.HTMLAttributes<HTMLDivElement>;
 
 export const Container: React.FC<Props> = forwardRef(
-  ({ children, maxWidth, ...props }: Props, ref: React.Ref<HTMLDivElement>) => {
+  (
+    {
+      children,
+      maxWidth,
+      flexDirection = 'row',
+      alignItems = 'center',
+      justifyContent = 'center',
+      ...props
+    }: Props,
+    ref: React.Ref<HTMLDivElement>,
+  ) => {
     return (
       <Wrapper ref={ref} {...props}>
-        <Inner className="inner" maxWidth={maxWidth}>
+        <Inner
+          className="inner"
+          maxWidth={maxWidth}
+          flexDirection={flexDirection}
+          alignItems={alignItems}
+          justifyContent={justifyContent}
+        >
           {children}
         </Inner>
       </Wrapper>
@@ -27,7 +46,13 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Inner = styled.div<Pick<Props, 'maxWidth'>>`
+const Inner = styled.div<
+  Pick<Props, 'maxWidth' | 'flexDirection' | 'justifyContent' | 'alignItems'>
+>`
   width: 100%;
   max-width: ${({ maxWidth }) => maxWidth ?? vars.breakpoints.md};
+  display: flex;
+  flex-direction: ${({ flexDirection }) => flexDirection};
+  justify-content: ${({ justifyContent }) => justifyContent};
+  align-items: ${({ alignItems }) => alignItems};
 `;
