d8abcae2b63737ce613c183d13d1409b76f37097
cla6shade <cla6shade@gmail.com>
2024-07-03 15:34:54 +0900

    feat: theme page에 대한 라우터 경로 설정

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from '@pages/main/MainPage';
 import Paths from '@constants/Paths';
+import ThemePage from '@pages/ThemePage';
 
 const router = createBrowserRouter([
   {
     path: Paths.MAIN_PAGE,
     element: <MainPage />,
+  },
+  {
+    path: Paths.THEME_PAGE(':themeKey'),
+    element: <ThemePage />,
   },
 ]);
 
