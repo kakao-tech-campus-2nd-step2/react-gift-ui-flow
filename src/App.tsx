98339e6690e32b574a2cafa0b19f4010416ffec4
dandamdandam <glue0440@gmail.com>
2024-07-03 14:44:36 +0900

    style: change a tab size 2 to 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import styled from '@emotion/styled';
 
 const App = () => {
-  const name = 'Josh Perez';
+    const name = 'Josh Perez';
 
-  return (
-    <div>
-      <Title>Hello, {name}</Title>
-    </div>
-  );
+    return (
+        <div>
+            <Title>Hello, {name}</Title>
+        </div>
+    );
 };
 
 export default App;
 
 const Title = styled.h1`
-  font-size: 1.5em;
-  color: gray;
+    font-size: 1.5em;
+    color: gray;
 `;
