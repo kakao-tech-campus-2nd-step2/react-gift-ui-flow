752d22606bd698441d84418f78e85bc58aa8eed8
gimdogyun <jasper12394@gmail.com>
2024-07-03 15:37:31 +0900

    feat : create Pages file

---
+++ src/pages/MyAccount.tsx
@@ -0,0 +1,9 @@
+function MyAccount() {
+  return (
+    <div>
+      <h1>My Account</h1>
+    </div>
+  );
+}
+
+export default MyAccount;
---
+++ src/pages/Theme.tsx
@@ -0,0 +1,9 @@
+function Theme() {
+  return (
+    <div>
+      <h1>Theme</h1>
+    </div>
+  );
+}
+
+export default Theme;
