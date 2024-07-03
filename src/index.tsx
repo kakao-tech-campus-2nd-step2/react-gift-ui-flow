09cf7b2c5fdacf35a77a8c7a3997f791eadba7e7
Dobbymin <gmin0701@knu.ac.kr>
2024-07-03 14:45:20 +0900

    feat(index.tsx): strictmode 제거 및 globalStyle 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import '@/styles';
 
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
+import { GlobalStyle } from '@/styles/GlobalStyles';
+import { Global } from '@emotion/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <>
+    <Global styles={GlobalStyle} />
     <App />
-  </React.StrictMode>,
+  </>,
 );
