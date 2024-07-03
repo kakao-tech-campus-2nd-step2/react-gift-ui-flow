7fee691be8584433f3cd59ebbc9b1a8e95ce25fa
cla6shade <cla6shade@gmail.com>
2024-07-03 14:53:21 +0900

    design: 버튼의 lightGray 테마 변경

--- src/styles/colors/index.ts
+++ src/styles/colors/index.ts
@@ -2,7 +2,7 @@ export const buttonColors = {
   kakao: 'rgb(254, 229, 0)',
   naver: 'rgb(40,205,0)',
   black: 'rgb(5,5,5)',
-  lightGray: 'rgb(138,138,138)',
+  lightGray: 'rgb(243,243,243)',
   darkGray: 'rgb(37,37,37)',
 };
 
@@ -18,7 +18,7 @@ export const buttonHoverColors = {
   kakao: 'rgb(255,217,0)',
   naver: 'rgb(87,255,41)',
   black: 'rgb(60,60,60)',
-  lightGray: 'rgb(174,174,174)',
+  lightGray: 'rgb(218,218,218)',
   darkGray: 'rgb(78,78,78)',
 };
 
