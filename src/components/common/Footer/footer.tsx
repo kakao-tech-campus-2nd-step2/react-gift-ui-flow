74becdb78dcfa9ace91d946269e82971df36808c
YIMSEBIN <jij09123@gmail.com>
2024-07-03 20:40:11 +0900

    feat: Footer 컴포넌트 구현 및 스토리북

---
+++ src/components/common/Footer/footer.tsx
@@ -0,0 +1,13 @@
+import '@/components/common/Footer/Footer.css';
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="text">
+          <p>카카오 선물하기</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
