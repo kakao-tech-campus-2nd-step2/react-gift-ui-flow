a24e35bffabdc89b8562010b3597f48d8fef7cae
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-07-03 15:17:31 +0900

    refactor:  improve Header reusability

--- src/pages/theme/Theme.tsx
+++ src/pages/theme/Theme.tsx
@@ -1,10 +1,15 @@
-import Banner from './components/Banner';
+import Header from './components/Header';
 import Items from './components/Items';
 
 const Theme = () => {
   return (
     <div>
-      <Banner />
+      <Header
+        label="가벼운 선물"
+        text="예산은 가볍게, 감동은 무겁게❤️"
+        description="당신의 센스를 뽐내줄 부담 없는 선물"
+        backgroundColor="#3e3e3e"
+      />
       <Items />
     </div>
   );
