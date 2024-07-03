0f6c014a2776351519a7c29e86248fffaed574a6
cla6shade <cla6shade@gmail.com>
2024-07-03 14:52:54 +0900

    design: AI 버튼 섹션이 padding을 가지도록 수정

--- src/components/organisms/main/AIButtonSection.tsx
+++ src/components/organisms/main/AIButtonSection.tsx
@@ -18,28 +18,32 @@ function AIButtonSection() {
 `;
 
   return (
-    <Container
-      elementSize="full-width"
-      justifyContent="center"
-    >
+    <Container elementSize="full-width" justifyContent="center">
       <Container
+        elementSize="full-width"
         maxWidth={MAX_CONTENT_WIDTH}
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-        backgroundColor={buttonColors.kakao} // 나중에 컬러 팔레트를 새로 만들던가 해야겠다..
-        elementSize={{
-          width: '100%',
-          height: '76px',
-        }}
         cssProps={{
-          borderRadius: '8px',
+          padding: '16px',
         }}
       >
-        <AIButtonCaption>AI가 추천하는 선물</AIButtonCaption>
-        <AIButtonCaptionStrong>선물을 추천받고 싶은 친구를 선택해주세요.</AIButtonCaptionStrong>
-      </Container>
+        <Container
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          backgroundColor={buttonColors.kakao} // 나중에 컬러 팔레트를 새로 만들던가 해야겠다..
+          elementSize={{
+            width: '100%',
+            height: '76px',
+          }}
+          cssProps={{
+            borderRadius: '8px',
+          }}
+        >
+          <AIButtonCaption>AI가 추천하는 선물</AIButtonCaption>
+          <AIButtonCaptionStrong>선물을 추천받고 싶은 친구를 선택해주세요.</AIButtonCaptionStrong>
+        </Container>
 
+      </Container>
     </Container>
   );
 }
