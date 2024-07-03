0d3ce4b100d2f4b073399f0ca6c400949369b08b
JeongSeoyun <xeonj@naver.com>
2024-07-03 18:29:21 +0900

    feat: 메인페이지 선물 받을 친구 배너 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,29 @@
 //import React from 'react'
 
+import './Main.css'
+
+import Footer from '@/components/common/Footer'
 import Header from '@/components/common/Header'
+import { Image } from '@/components/common/Image'
+
+import choonsik from '../images/choonsik.png'
 
 const Main = () => {
   return (
     <div className='MainPage'>
-        <Header title='선물하기'/>
+        <Header title='선물하기' subtitle='로그인'/>
+        <div className='selectFriend'>
+            <Image
+                src={choonsik}
+                alt='choonsik'
+                radius={20}
+                ratio={'square'}
+                width={70}
+                height={70}
+             />
+            <div className='selectFriendTitle'>선물 받을 친구를 선택해주세요.</div>
+        </div>
+        <Footer title='카카오톡 선물하기' />
     </div>
   )
 }
