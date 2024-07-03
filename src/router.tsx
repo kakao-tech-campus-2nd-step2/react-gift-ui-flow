c0e3110a15ab06845f967cc34d448d58eaa2efc9
Dobbymin <gmin0701@knu.ac.kr>
2024-07-03 15:04:26 +0900

    feat(router): Outlet을 활용한 header, footer 유지 및 하위 페이지 경로 설정

---
+++ src/router.tsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter, Outlet } from 'react-router-dom';
+
+import { AUTH, PATHS } from '@/constants/Path';
+import LoginPage from '@/pages/login/LoginPage';
+import MainPage from '@/pages/main/MainPage';
+import ThemePage from '@/pages/theme/ThemePage';
+import MyAccountPage from '@/pages/my-account/MyAccountPage';
+import Layout from '@/components/layout/Layout';
+
+export const router = createBrowserRouter([
+  {
+    path: PATHS.MAIN,
+    element: (
+      <Layout>
+        <Outlet />
+      </Layout>
+    ),
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: PATHS.THEME,
+        element: <ThemePage />,
+      },
+    ],
+  },
+  {
+    path: AUTH.LOGIN,
+    element: <LoginPage />,
+  },
+  {
+    path: AUTH.MY_ACCOUNT,
+    element: <MyAccountPage />,
+  },
+]);
