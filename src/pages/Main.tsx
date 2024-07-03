3986b3f8bfc1b9713c6c49e95978b637632506af
JeongSeoyun <xeonj@naver.com>
2024-07-03 14:58:02 +0900

    feat: Main 페이지 추가

---
+++ src/pages/Main.tsx
@@ -0,0 +1,13 @@
+//import React from 'react'
+
+import Header from '@/components/common/Header'
+
+const Main = () => {
+  return (
+    <div className='MainPage'>
+        <Header title='선물하기'/>
+    </div>
+  )
+}
+
+export default Main
