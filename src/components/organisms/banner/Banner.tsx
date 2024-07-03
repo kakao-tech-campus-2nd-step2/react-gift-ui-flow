a111b40496ebbfeaf9188b58af0bc18afef6dec2
cla6shade <cla6shade@gmail.com>
2024-07-03 15:34:38 +0900

    design: 배너가 최대 영역으로 넓어지지 못하던 문제점 해결

--- src/components/organisms/banner/Banner.tsx
+++ src/components/organisms/banner/Banner.tsx
@@ -1,6 +1,7 @@
 import Container from '@components/atoms/container/Container';
 import { backgroundColors } from '@styles/colors';
 import GiftThemes from '@constants/GiftThemes';
+import { MAX_CONTENT_WIDTH } from '@styles/size';
 import { GiftThemeKey } from '@/types';
 import { ThemeName, ThemeSubtitle, ThemeTitle } from './Banner.styles';
 
@@ -12,17 +13,19 @@ function Banner({ themeKey }: BannerProps) {
   const themeTexts = GiftThemes[themeKey];
 
   return (
-    <Container backgroundColor={backgroundColors.containerDark} padding="50px 20px">
-      <Container maxWidth="1100px" flexDirection="column">
-        <ThemeName>
-          {themeTexts.keyword}
-        </ThemeName>
-        <ThemeTitle>
-          {themeTexts.title}
-        </ThemeTitle>
-        <ThemeSubtitle>
-          {themeTexts.subtitle}
-        </ThemeSubtitle>
+    <Container backgroundColor={backgroundColors.containerDark} elementSize="full-width" justifyContent="center">
+      <Container maxWidth={MAX_CONTENT_WIDTH} elementSize="full-width" padding="50px 20px">
+        <Container flexDirection="column">
+          <ThemeName>
+            {themeTexts.keyword}
+          </ThemeName>
+          <ThemeTitle>
+            {themeTexts.title}
+          </ThemeTitle>
+          <ThemeSubtitle>
+            {themeTexts.subtitle}
+          </ThemeSubtitle>
+        </Container>
       </Container>
     </Container>
   );
