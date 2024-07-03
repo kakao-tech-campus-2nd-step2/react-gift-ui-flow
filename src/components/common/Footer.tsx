e269f5a995ef706185ce4993b78f613575b0584a
gimdogyun <jasper12394@gmail.com>
2024-07-03 18:45:01 +0900

    design : Header, Footer design

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <footer className="mainLayout__footer">
-      <div className="mainLayout__footer__contents">
+    <footer className="mainLayout__footer__frame">
+      <div className="mainLayout__footer">
         <p>카카오톡 선물하기</p>
       </div>
     </footer>
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,14 +2,16 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="mainLayout__header">
-      <Link to="/">
-        <p className="mainLayout__header__logo">선물하기</p>
-      </Link>
-      <Link to="/login">
-        <p className="mainLayout__header__login">로그인</p>
-      </Link>
-    </header>
+    <div className="mainLayout__header__frame">
+      <header className="mainLayout__header">
+        <Link to="/">
+          <p className="mainLayout__header__logo">선물하기</p>
+        </Link>
+        <Link to="/login">
+          <p className="mainLayout__header__login">로그인</p>
+        </Link>
+      </header>
+    </div>
   );
 }
 
