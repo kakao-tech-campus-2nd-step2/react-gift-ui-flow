d746cf3af35ae065094a533a969f048c32914c2d
minji2219 <200146@jnu.ac.kr>
2024-07-03 16:15:14 +0900

    Feat: 불필요한 텍스트 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import styled from '@emotion/styled';
-
 import { RoutesPage } from './components/Routes';
 
 const App = () => {
-  const name = 'Josh Perez';
-
   return (
     <div>
       <RoutesPage />
-      <Title>Hello, {name}</Title>
     </div>
   );
 };
 
 export default App;
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  color: gray;
-`;
