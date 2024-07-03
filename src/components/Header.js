916238277c82c02d2f576fe91febb778de4a7ddd
HyeonChae1104 <211063@jnu.ac.kr>
2024-07-03 15:51:39 +0900

    feat: Header 추가

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="my-account">My Account</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
