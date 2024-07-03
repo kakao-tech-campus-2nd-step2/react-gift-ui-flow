30b7721df4f99a4a6fd3004abf72f4dfd93b4002
JeongSeoyun <xeonj@naver.com>
2024-07-04 01:26:56 +0900

    feat: 메인페이지 랭킹 추천기준 섹션 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,6 +2,8 @@
 
 import './Main.css'
 
+import { useState } from 'react'
+
 import { Button } from '@/components/common/Button'
 import Footer from '@/components/common/Footer'
 import Header from '@/components/common/Header'
@@ -26,6 +28,22 @@ const Main = () => {
         {id: 11, img: category01, text: '과일/한우'},
         {id: 12, img: category01, text: '출산/키즈'}
     ]
+
+    const [selectedItemWho, setSelectedItemWho] = useState<string>('all');
+
+    const handleSelectWho = (item: string) => {
+        setSelectedItemWho(item);
+    };
+
+
+    const [selectedItemWhich, setSelectedItemWhich] = useState<string>('받고 싶어한');
+
+    const handleSelectWhich = (item: string) => {
+        setSelectedItemWhich(item);
+    };
+
+    
+
   return (
     <div className='MainPage'>
         <Header title='선물하기' subtitle='로그인'/>
@@ -63,6 +81,33 @@ const Main = () => {
                 <div className='buttonSubtitle'>AI가 추천하는 선물</div>
                 <div className='buttonTitle'>선물을 추천받고 싶은 친구를 선택해주세요.</div>
             </Button>
+        </div>
+
+        <div className='rankingContainer'>
+            <div className='rankingTitle'>실시간 급상승 선물랭킹</div>
+            <div className='rankingSelectWhoContainer'>
+                <div className={`rankingWhoItemContainer ${selectedItemWho === 'all' ? 'selectedItemWho' : ''}`} onClick={() => handleSelectWho('all')}>
+                    <div className='whoItemImg'>ALL</div>
+                    <div className='whoItemText'>전체</div>
+                </div>
+                <div className={`rankingWhoItemContainer ${selectedItemWho === 'woman' ? 'selectedItemWho' : ''}`} onClick={() => handleSelectWho('woman')}>
+                    <div className='whoItemImg'>👩🏻‍🦳</div>
+                    <div className='whoItemText'>여성이</div>
+                </div>
+                <div className={`rankingWhoItemContainer ${selectedItemWho === 'man' ? 'selectedItemWho' : ''}`} onClick={() => handleSelectWho('man')}>
+                    <div className='whoItemImg'>👨🏻‍🦳</div>
+                    <div className='whoItemText'>남성이</div>
+                </div>
+                <div className={`rankingWhoItemContainer ${selectedItemWho === 'children' ? 'selectedItemWho' : ''}`} onClick={() => handleSelectWho('children')}>
+                    <div className='whoItemImg'>👦🏻</div>
+                    <div className='whoItemText'>청소년이</div>
+                </div>
+            </div>
+            <div className='rankingSelectBox'>
+                <div className={`rankingBoxItem ${selectedItemWhich === '받고 싶어한' ? 'selectedItemWhich' : ''}`} onClick={() => handleSelectWhich('받고 싶어한')}>받고 싶어한</div>
+                <div className={`rankingBoxItem ${selectedItemWhich === '많이 선물한' ? 'selectedItemWhich' : ''}`} onClick={() => handleSelectWhich('많이 선물한')}>많이 선물한</div>
+                <div className={`rankingBoxItem ${selectedItemWhich === '위시로 받은' ? 'selectedItemWhich' : ''}`} onClick={() => handleSelectWhich('위시로 받은')}>위시로 받은</div>
+            </div>
         </div>
         
         <Footer title='카카오톡 선물하기'  />
