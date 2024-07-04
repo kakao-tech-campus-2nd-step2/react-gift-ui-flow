60da177d5635d57d0ee9db920aa2c97bb9e5ca6b
seung365 <bdh3659@naver.com>
2024-07-04 12:12:49 +0900

    feat: RouterInfo 구현

---
+++ src/routes/RouterInfo.tsx
@@ -0,0 +1,54 @@
+import MainPage from '@/pages/MainPage';
+import LoginPage from '@/pages/LoginPage';
+import Mypage from '@/pages/MyPage';
+import Gift from '@/pages/Gift';
+import { createBrowserRouter } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+const Layout = () => {
+  // header와 footer에 가리지 않에 padding 추가
+  return (
+    <Container>
+      <Header />
+      <main style={{ paddingBottom: '60px', paddingTop: '60px' }}>
+        <Outlet />
+      </main>
+      <Footer />
+    </Container>
+  );
+};
+
+const RouterInfo = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: '/my-account',
+        element: <Mypage />,
+      },
+      {
+        path: '/theme/:themekey',
+        element: <Gift />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+]);
+
+export default RouterInfo;
