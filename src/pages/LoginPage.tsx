8cf7df4002699e18f2c1cbc3376c38d86bc2ad41
cla6shade <cla6shade@gmail.com>
2024-07-04 23:51:27 +0900

    feat: 로그인 화면에서 로그인된 사용자의 경우 메인 페이지로 이동하도록 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,8 +2,21 @@ import LoginForm from '@components/organisms/login/LoginForm';
 import Container from '@components/atoms/container/Container';
 import { css } from '@emotion/react';
 import loginLogo from '@assets/images/login_logo.svg';
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Paths from '@constants/Paths';
+import { LoginContext } from '@/providers/LoginContextProvider';
 
 function LoginPage() {
+  const navigate = useNavigate();
+  const { isLoggedIn } = useContext(LoginContext);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate(Paths.MAIN_PAGE);
+    }
+  }, [navigate, isLoggedIn]);
+
   return (
     <Container
       elementSize={{
