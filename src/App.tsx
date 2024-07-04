45f3f35f57ef32a5f3ab8df098e6f37234fd434c
Kuheewon <selly3109@naver.com>
2024-07-04 15:07:05 +0900

    refactor. 불필요한 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ const App = () => {
         title="선물하기"
         imageUrl="https://gift-s.kakaocdn.net/dn/gift/images/m640/pc_gift_logo.png"
       />
-      <div style={{ padding: '100px 0' }}>{/* Main content goes here */}</div>
       <Footer content="카카오톡 선물하기" />
     </div>
   );
