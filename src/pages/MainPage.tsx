71d175c9f4b1c8aad402f6c9d8bb8bd8ea73022b
Da-Yeon-Kim <210941@jnu.ac.kr>
2024-07-05 04:31:10 +0900

    feat(MainPage): Friend 섹션 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,29 @@
+import styled from '@emotion/styled';
+
+import { Image } from '@/components/common/Image/index';
+
 export const MainPage: React.FC = () => {
   return (
     <>
-      <p>MainPage</p>
+      <FriendSection>
+        <FriendContainer>
+          <FriendImage
+            src="https://gift-s.kakaocdn.net/dn/gift/images/m640/bg_profile_default.png"
+            alt="선물 받을 친구"
+            radius={25}
+            ratio="square"
+          />
+          <FriendText>선물 받을 친구를 선택해주세요.</FriendText>
+        </FriendContainer>
+      </FriendSection>
     </>
   );
 };
+
+const FriendSection = styled.section``;
+
+const FriendContainer = styled.div``;
+
+const FriendImage = styled(Image)``;
+
+const FriendText = styled.span``;
