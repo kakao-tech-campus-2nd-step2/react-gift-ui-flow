0ecfbba0e109b414b6744318d2a95023d54ae709
cla6shade <cla6shade@gmail.com>
2024-07-04 23:47:14 +0900

    feat: 인풋 컴포넌트가 ref를 전달받을 수 있도록 변경

--- src/components/atoms/input/Input.tsx
+++ src/components/atoms/input/Input.tsx
@@ -1,25 +1,25 @@
 import { StyledErrorMessage, StyledInput } from '@components/atoms/input/Input.styles';
+import { forwardRef } from 'react';
 import { InputProps } from '@/types';
 
-function Input({
-  disabled, invalid, elementSize, errorMessage, ...rest
-}: InputProps) {
-  return (
-    <>
-      <StyledInput
-        elementSize={elementSize}
-        disabled={disabled}
-        {...rest}
-      />
-      {invalid
-        ? (
-          <StyledErrorMessage>
-            {errorMessage ?? '잘못된 입력값입니다.'}
-          </StyledErrorMessage>
-        )
-        : null}
-    </>
-  );
-}
+const Input = forwardRef<HTMLInputElement, InputProps>(({
+  elementSize, invalid, errorMessage, disabled, ...rest
+}, ref) => (
+  <>
+    <StyledInput
+      elementSize={elementSize}
+      disabled={disabled}
+      ref={ref}
+      {...rest}
+    />
+    {invalid
+      ? (
+        <StyledErrorMessage>
+          {errorMessage ?? '잘못된 입력값입니다.'}
+        </StyledErrorMessage>
+      )
+      : null}
+  </>
+));
 
 export default Input;
