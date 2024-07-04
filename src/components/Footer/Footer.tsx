94dfea977df7e83b415cc7ffcf5ade83ca789400
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-07-04 15:44:51 +0900

    feat : Footer 컴포넌트 예시와 같이 수정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <p>(주)카카오대표이사 : 정신아주소 : 제주특별자치도 제주시 첨단로 242</p>
+      <p>카카오톡 선물하기</p>
     </footer>
   );
 };
