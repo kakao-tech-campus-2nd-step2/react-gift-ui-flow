d3954a2aa28c6108e4fa976bd378d195bdbb801e
yuni <hys990730@gmail.com>
2024-07-04 22:45:00 +0900

    feat: ThemePage 기본 구조 생성

---
+++ src/pages/ThemePage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const ThemePage = () => {
+  const { themeKey } = useParams<{ themeKey: string }>();
+
+  return (
+    <div>
+      <h2>Theme Page</h2>
+      <p>Current Theme: {themeKey}</p>
+      {/* 추가적인 테마 페이지 컨텐츠 구현 */}
+    </div>
+  );
+};
+
+export default ThemePage;
