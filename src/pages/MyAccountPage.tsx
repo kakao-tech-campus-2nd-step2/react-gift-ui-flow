0885eccf286f81a7465845481a20ace49083ccd8
Da-Yeon-Kim <210941@jnu.ac.kr>
2024-07-05 05:22:08 +0900

    feat(MyAccountPage): 내 계정 페이지 구현

--- src/pages/MyAccountPage.tsx
+++ src/pages/MyAccountPage.tsx
@@ -1,7 +1,21 @@
+import styled from '@emotion/styled';
+
+import { Button } from '@/components/common/Button';
+import { Container } from '@/components/common/layouts/Container/index';
+
 export const MyAccountPage: React.FC = () => {
   return (
-    <>
-      <p>MyAccountPage</p>
-    </>
+    <MySection>
+      <h1>안녕하세요!</h1>
+      <MyContainer>
+        <Button theme="darkGray" size="small">
+          <strong>로그아웃</strong>
+        </Button>
+      </MyContainer>
+    </MySection>
   );
 };
+
+const MySection = styled.section``;
+
+const MyContainer = styled(Container)``;
