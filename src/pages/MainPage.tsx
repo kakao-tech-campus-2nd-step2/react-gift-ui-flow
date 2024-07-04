811af56e9a6ef41ea9ed0184c1a0489f59cfa1ce
yuni <hys990730@gmail.com>
2024-07-04 22:42:59 +0900

    feat: MainPage 컴포넌트 추가

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Main Page</h1>
+      <p>Welcome to the main page!</p>
+    </div>
+  );
+};
+
+export default MainPage;
