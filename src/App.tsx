27b936f57366cd4a495b465a66ba1fd207b1e858
yuni <hys990730@gmail.com>
2024-07-04 22:40:24 +0900

    feat: App 구조 설정
    - Header 컴포넌트를 애플리케이션 상단에 추가
    - react-router-dom의 Outlet을 사용하여 동적 라우팅 구현
    - Footer 컴포넌트를 애플리케이션 하단에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
 import styled from '@emotion/styled';
+
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const App = () => {
   const name = 'Josh Perez';
