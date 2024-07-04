fad18372e76709c1f7bafc0103902d6d90278aa3
joshuadesu <meron_pan@kakao.com>
2024-07-04 15:40:01 +0900

    Feat: Add Router 및 링크

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,61 @@
-import styled from '@emotion/styled';
+// import styled from '@emotion/styled';
+import styled from "@emotion/styled"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const App = () => {
-  const name = 'Josh Perez';
+import Footer from '@/components/common/Footer/index';
+import Header from "@/components/common/Header/index";
+import Homepage from '@/pages/Homepage/Homepage';
+import MyAccountPage from "@/pages/My-account/MyAccountPage"
+import SignInPage from "@/pages/Sign-in/SignInPage";
 
+
+const App: React.FC = () => {
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     children: [
+  //       { path: "", element: <Homepage /> },
+  //       {
+  //         path:"login"
+  //         element: <SignIn />
+  //       },
+  //       {
+  //         path: "my-account",
+  //         element: <MyAccount />
+  //       },
+  //     ]
+  //   }
+  // ])
   return (
-    <div>
-      <Title>Hello, {name}</Title>
-    </div>
+    <>
+    <BrowserRouter>
+      <Header></Header>
+      <Body>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/login" element={<SignInPage />}></Route>
+          <Route path="/my-account" element={<MyAccountPage />}></Route>
+        </Routes>
+      </Body>
+      <Footer></Footer>
+    </BrowserRouter>
+    </>
   );
 };
 
 export default App;
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  color: gray;
-`;
+const Body = styled.div`
+  padding-top: 30px;
+  width: 100vw;
+  height: 90vh;
+  align-items: center;
+  justify-content: center;
+`
+
+// style?
+
+// const Title = styled.h1`
+//   font-size: 1.5em;
+//   color: gray;
+// `;
