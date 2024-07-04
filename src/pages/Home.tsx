0de00fb975daf813d5c1c3e7fbc78422f440e29e
박건규 <geongyu09@gmail.com>
2024-07-04 22:28:06 +0900

    refactor: banner의 공통적으로 사용할 수 있는 부분을 공통 컴포넌트로 분리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,17 @@
-import HomeBanner from "@/components/Banner/HomeBanner";
-import Header from "@/components/Header";
 import CatetoryGrid from "@/components/Category/CategoryGrid";
 import RecommandBtn from "@/components/RecommandBtn";
 import RealTimeRanking from "@/components/RealTimeRanking";
-import Footer from "@/components/Footer";
-import { css } from "@emotion/css";
+import PageLayout from "@/components/common/layouts/PageLayout";
+import HomeBanner from "@/components/common/Banner/HomeBanner";
 
 const Home = () => {
   return (
-    <div>
-      <Header />
+    <PageLayout>
       <HomeBanner />
       <CatetoryGrid />
       <RecommandBtn />
       <RealTimeRanking />
-      <div
-        className={css`
-          margin-top: 8rem;
-        `}
-      >
-        <Footer />
-      </div>
-    </div>
+    </PageLayout>
   );
 };
 
