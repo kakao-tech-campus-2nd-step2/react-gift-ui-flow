7118b5f6b330e65ec34680b2a48667859a3d619a
yuni <hys990730@gmail.com>
2024-07-04 22:44:20 +0900

    feat: MyAccountPage 기본 구조 생성

---
+++ src/pages/MyAccountPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MyAccountPage = () => {
+  const handleLogout = () => {
+    // 로그아웃 처리 로직
+    console.log('로그아웃 처리');
+  };
+
+  return (
+    <div>
+      <h2>My Account Page</h2>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
+
+export default MyAccountPage;
