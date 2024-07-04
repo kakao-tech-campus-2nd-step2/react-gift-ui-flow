7108044c4f84d177fa01ef71f8a7ec54e8f34b8e
JeongSeoyun <xeonj@naver.com>
2024-07-05 00:28:38 +0900

    feat: 로그인 로직 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,34 @@
-//import React from 'react'
 import './Login.css'
+
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 import { Button } from "@/components/common/Button"
 import { UnderlineTextField } from "@/components/common/Form/Input/UnderlineTextField"
 
 const Login = () => {
+
+    const history = useNavigate()
+    const [id, setId] = useState('')
+    const [pw, setPw] = useState('')
+
+    const handleLogin = () => {
+        if (id&&pw) { // id와 비밀번호가 모두 입력되었을 때만 로그인 처리
+            sessionStorage.setItem('authToken', id)
+            history(-1)
+        } else {
+            alert('ID와 비밀번호를 모두 입력해주세요.')
+        }
+
+    }
+
   return (
     <div className="Login">
         <div className="title">Kakao</div>
         <div className="container">
-            <UnderlineTextField placeholder='이름' className='textFieldId'/>
-            <UnderlineTextField placeholder='비밀번호' className='textFieldPw'/>
-            <Button theme='kakao' size='responsive' className='buttonLogin'>
+            <UnderlineTextField placeholder='이름' value={id} onChange={(e) => setId((e.target as HTMLInputElement).value)} className='textFieldId'/>
+            <UnderlineTextField placeholder='비밀번호' value={pw} onChange={(e) => setPw((e.target as HTMLInputElement).value)} className='textFieldPw'/>
+            <Button theme='kakao' size='responsive' onClick={handleLogin} className='buttonLogin'>
                 <div>로그인</div>
             </Button>
         </div>
