45c34c40ce2f52b9b30fde033034ff943fa02366
cla6shade <cla6shade@gmail.com>
2024-07-04 23:27:13 +0900

    refactor: LoginContextProvider로 router 감싸기

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import router from '@router';
 import { RouterProvider } from 'react-router-dom';
 import GlobalStyle from '@styles/GlobalStyle';
+import LoginContextProvider from '@/providers/LoginContextProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalStyle />
-    <RouterProvider router={router} />
+    <LoginContextProvider>
+      <RouterProvider router={router} />
+    </LoginContextProvider>
   </React.StrictMode>,
 );
