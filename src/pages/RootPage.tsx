55987ea8ab624472c4f7e270790672e2296bee78
KimKyuHoi <k546kh@gmail.com>
2024-07-04 15:16:29 +0900

    Design: Container 컴포넌트 교체

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,11 +1,12 @@
 import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
 
+import { Container } from '@/components/common/layouts/Container';
 import MainFooter from '@/components/ui/footer/MainFooter';
 import MainNavigation from '@/components/ui/header/MainNavigation';
 
-const MainContent = styled.main`
-  width: 100vw;
+const MainContent = styled(Container)`
+  width: 100%;
   height: auto;
   overflow: hidden;
 `;
@@ -14,7 +15,7 @@ const RootPage = () => {
   return (
     <>
       <MainNavigation />
-      <MainContent>
+      <MainContent maxWidth="100vw">
         <Outlet />
       </MainContent>
       <MainFooter />
