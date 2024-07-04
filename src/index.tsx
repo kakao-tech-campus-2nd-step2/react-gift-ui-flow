0bc7490a57dcd6a8a9484cb8ac993e336c76938d
yuni <hys990730@gmail.com>
2024-07-04 22:38:52 +0900

    feat: 라우터 설정을 위해 RouterProvider 사용
    - `react-router-dom`의 RouterProvider를 사용하여 라우터 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import '@/styles';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from '@/Router';
 
 import App from '@/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 );
