940c4d85fa1cf35d8f4761cbf7f901fd3836b9c2
Kuheewon <selly3109@naver.com>
2024-07-04 18:54:56 +0900

    feat. 테마 섹션 추가하기

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import ThemePage from './ThemePage';
+
 const HomePage: React.FC = () => {
   return (
     <div>
       <h2>Home Page</h2>
       <p>This is the home page.</p>
+      <ThemePage />
     </div>
   );
 };
