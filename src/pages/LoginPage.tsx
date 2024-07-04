fe6cae91b533089210f425d398a97200920209a8
yuni <hys990730@gmail.com>
2024-07-05 02:23:40 +0900

    feat: LoginPage에 UnderlineTextField 및 Button 컴포넌트 적용
    - LoginPage에서 기존 <input>을 UnderlineTextField 컴포넌트로 교체
    - 로그인 버튼을 스타일링된 Button 컴포넌트로 교체

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,33 +1,38 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+
+import { Button } from '@/components/common/Button';
+import { UnderlineTextField } from '@/components/common/Form/Input/UnderlineTextField';
 
 const LoginPage = () => {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    alert(`Logged in with ID: ${id} and Password: ${password}`);
+  const handleLogin = () => {
+    alert(`로그인 처리`);
   };
 
   return (
     <div>
       <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="id">ID:</label>
-          <input type="text" id="id" value={id} onChange={(e) => setId(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="password">PW:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      <div>
+        <UnderlineTextField
+          type="text"
+          id="id"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
+      </div>
+      <div>
+        <UnderlineTextField
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <Button onClick={handleLogin} type="submit" theme="kakao" size="responsive">
+        로그인
+      </Button>
     </div>
   );
 };
--- src/pages/MyAccountPage.tsx
+++ src/pages/MyAccountPage.tsx
@@ -2,14 +2,13 @@ import { Button } from '@/components/common/Button';
 
 const MyAccountPage = () => {
   const handleLogout = () => {
-    console.log('로그아웃 처리');
+    alert(`로그아웃 처리`);
   };
 
   return (
     <div>
-      <h2>My Account Page</h2>
       <Button onClick={handleLogout} theme="outline" size="responsive">
-        Logout
+        로그아웃
       </Button>
     </div>
   );
