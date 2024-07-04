b4be61c5c12e3a3b054ed3d19738a3a095d1f0e1
yuni <hys990730@gmail.com>
2024-07-05 01:07:58 +0900

    feat: MyAccountPage에 로그아웃 버튼 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,35 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const LoginPage: React.FC = () => {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    alert(`Logged in with ID: ${id} and Password: ${password}`);
-  };
-
+const MyAccountPage: React.FC = () => {
   return (
     <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="id">ID:</label>
-          <input type="text" id="id" value={id} onChange={(e) => setId(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="password">PW:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      <h1>My Account</h1>
+      <button>Logout</button>
     </div>
   );
 };
 
-export default LoginPage;
+export default MyAccountPage;
