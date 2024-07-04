2862669b54761b24028166559ddd9d06520de8b3
Dobbymin <gmin0701@knu.ac.kr>
2024-07-04 12:07:24 +0900

    feat(main): 선물랭킹 데이터 작성

---
+++ src/constants/RankData.ts
@@ -0,0 +1,182 @@
+import menuImg from '@/assets/main/Gift_Icon.jpg';
+
+export const targetData = [
+  {
+    buttonTitle: 'ALL',
+    title: '전체',
+  },
+  {
+    buttonTitle: '👩🏻‍🦳',
+    title: '여성이',
+  },
+  {
+    buttonTitle: '👨🏻‍🦳',
+    title: '남성이',
+  },
+  {
+    buttonTitle: '👦🏻',
+    title: '청소년이',
+  },
+];
+
+export const giftTypeData = [
+  {
+    type: '받고 싶어한',
+  },
+  {
+    type: '많이 선물한',
+  },
+  {
+    type: '위시로 받은',
+  },
+];
+
+export const RankData = [
+  {
+    rank: 1,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 2,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 3,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 4,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 5,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 6,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 7,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 8,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 9,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 10,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 11,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 12,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 13,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 14,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 15,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 16,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 17,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 18,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 19,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 20,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+  {
+    rank: 20,
+    Img: menuImg,
+    storeTitle: 'BBQ',
+    menu: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: '29,000원',
+  },
+];
