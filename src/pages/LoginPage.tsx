05f670d16f50241447900430e5cbad1fe22e3782
yuni <hys990730@gmail.com>
2024-07-05 01:06:22 +0900

    feat: LoginPage에 ID와 PW 입력 폼 생성

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,19 +1,26 @@
-const LoginPage = () => {
+import React, { useState } from 'react';
+
+const LoginPage: React.FC = () => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
-      <h2>Login Page</h2>
+      <h1>Login</h1>
       <form>
-        <label>
-          ID:
-          <input type="text" />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" />
-        </label>
-        <br />
-        <button type="submit">Login</button>
+        <div>
+          <label htmlFor="id">ID:</label>
+          <input type="text" id="id" value={id} onChange={(e) => setId(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="password">PW:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
       </form>
     </div>
   );
