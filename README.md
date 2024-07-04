c0a5156c12e0dcb87ac316cf04c35fa3846d4c4c
Da-Yeon-Kim <210941@jnu.ac.kr>
2024-07-05 05:08:40 +0900

    feat(ThemePage): 헤더 및 상품 목록 섹션 추가

--- README.md
+++ README.md
@@ -26,9 +26,8 @@ FE 카카오 선물하기 2주차 과제: 페이지 구현
 
 ### Theme 페이지(**`/theme/:themeKey`**)
 
-- [ ] Header 섹션 추가
+- [x] Header 및 상품 목록 섹션 추가
 - [ ] themeKey에 따라 label, title, description, backgroundColor 변화
-- [ ] 상품 목록 섹션 추가
 
 ### 로그인 페이지(**`/login`**)
 
--- src/pages/ThemePage.tsx
+++ src/pages/ThemePage.tsx
@@ -1,7 +1,48 @@
+import styled from '@emotion/styled';
+
+import { DefaultGoodsItems } from '@/components/common/GoodsItem/Default';
+import { Container } from '@/components/common/layouts/Container/index';
+import { Grid } from '@/components/common/layouts/Grid/index';
+
 export const ThemePage: React.FC = () => {
+  const ItemsData = [];
+  for (let i = 1; i <= 21; i++) {
+    ItemsData.push({
+      imageSrc:
+        'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+      subtitle: 'BBQ',
+      title: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+      amount: 29000,
+    });
+  }
   return (
     <>
-      <p>ThemePage</p>
+      <HeaderSection>
+        <HeaderContainer>
+          <HeaderLabel>Label</HeaderLabel>
+          <HeaderTitle>Title</HeaderTitle>
+          <HeaderDescription>Description</HeaderDescription>
+        </HeaderContainer>
+      </HeaderSection>
+      <ThemeSection>
+        <Grid columns={{ sm: 2, lg: 4 }} gap={20}>
+          {ItemsData.map((item, index) => (
+            <DefaultGoodsItems key={index} {...item} />
+          ))}
+        </Grid>
+      </ThemeSection>
     </>
   );
 };
+
+const HeaderSection = styled.section``;
+
+const HeaderContainer = styled(Container)``;
+
+const HeaderLabel = styled.p``;
+
+const HeaderTitle = styled.h1``;
+
+const HeaderDescription = styled.p``;
+
+const ThemeSection = styled.section``;
