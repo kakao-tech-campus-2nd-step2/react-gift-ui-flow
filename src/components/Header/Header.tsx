b89dc85267504a80a2f426d20c7e6c5d4f43e7fd
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-07-04 15:44:13 +0900

    feat : Header 컴포넌트 예시와 같이 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,20 +3,13 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <h1>선물하기</h1>
       <nav>
         <ul>
           <li>
-            <a href="/">홈</a>
+            <a href="/">선물하기</a>
           </li>
           <li>
-            <a href="/wish">위시</a>
-          </li>
-          <li>
-            <a href="/present">선물함</a>
-          </li>
-          <li>
-            <a href="/category">카테고리</a>
+            <a href="/login">로그인</a>
           </li>
         </ul>
       </nav>
