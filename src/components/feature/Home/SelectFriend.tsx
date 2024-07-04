70438c5c8c08daf1ddb7a45d18a079a799ab26e5
Hyoeunkh <lhe4528@knu.ac.kr>
2024-07-04 15:49:04 +0900

    feat: MainPage의 SelectFriend 컴포넌트 구현

---
+++ src/components/feature/Home/SelectFriend.tsx
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+
+import { Image } from '@/components/common/Image';
+import { Container } from '@/components/common/Layout/Container';
+
+export const SelectFriend = () => {
+  return (
+    <FriendWrapper>
+      <Container flexDirection="row" alignItems="center" maxWidth="1024px">
+        <ImageCustom
+          src="https://gift-s.kakaocdn.net/dn/gift/images/m640/bg_profile_default.png"
+          alt="친구 선택"
+          radius={24}
+          ratio={'square'}
+          onClick={() => alert('선물 받을 친구 선택하기')}
+        />
+        <Text>선물 받을 친구를 선택해주세요.</Text>
+      </Container>
+    </FriendWrapper>
+  );
+};
+const FriendWrapper = styled.section`
+  width: 100%;
+  height: 150px;
+  background-color: rgb(250, 250, 250);
+  padding: 40px 20px;
+`;
+const ImageCustom = styled(Image)`
+  width: 70px;
+  height: 70px;
+  margin-right: 16px;
+  cursor: pointer;
+`;
+const Text = styled.p`
+  font-size: 28px;
+`;
