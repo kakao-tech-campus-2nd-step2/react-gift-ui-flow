c62ddb12f5bceb88f426c556c9cdddb42fddb285
Da-Yeon-Kim <210941@jnu.ac.kr>
2024-07-05 05:16:28 +0900

    feat(LoginPage): 로그인 페이지 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,28 @@
+import styled from '@emotion/styled';
+
+import { Button } from '@/components/common/Button';
+import { UnderlineTextField } from '@/components/common/Form/Input/UnderlineTextField';
+import { Container } from '@/components/common/layouts/Container/index';
+
 export const LoginPage: React.FC = () => {
   return (
-    <>
-      <p>LoginPage</p>
-    </>
+    <LoginSection>
+      <h1>kakao</h1>
+      <FormContainer>
+        <LoginField placeholder="이름"></LoginField>
+        <LoginField placeholder="비밀번호" type="password"></LoginField>
+        <LoginButton theme="kakao" size="small">
+          로그인
+        </LoginButton>
+      </FormContainer>
+    </LoginSection>
   );
 };
+
+const LoginSection = styled.section``;
+
+const FormContainer = styled(Container)``;
+
+const LoginField = styled(UnderlineTextField)``;
+
+const LoginButton = styled(Button)``;
