d5dc1f287ccf063779fa143fe93481281ee5a991
Dobbymin <gmin0701@knu.ac.kr>
2024-07-04 12:07:42 +0900

    feat(main): 선물 종류 데이터 작성

---
+++ src/constants/GiftData.ts
@@ -0,0 +1,52 @@
+import GiftImg from '@/assets/main/Item_Icon.jpeg';
+
+export const TypeData = [
+  {
+    title: '생일',
+    image: GiftImg,
+  },
+  {
+    title: '졸업선물',
+    image: GiftImg,
+  },
+  {
+    title: '스몰럭셔리',
+    image: GiftImg,
+  },
+  {
+    title: '명품선물',
+    image: GiftImg,
+  },
+  {
+    title: '결혼/집들이',
+    image: GiftImg,
+  },
+  {
+    title: '따뜻한선물',
+    image: GiftImg,
+  },
+  {
+    title: '가벼운선물',
+    image: GiftImg,
+  },
+  {
+    title: '팬심저격',
+    image: GiftImg,
+  },
+  {
+    title: '교환권',
+    image: GiftImg,
+  },
+  {
+    title: '건강/비타민',
+    image: GiftImg,
+  },
+  {
+    title: '과일/한우',
+    image: GiftImg,
+  },
+  {
+    title: '출산/키즈',
+    image: GiftImg,
+  },
+];
