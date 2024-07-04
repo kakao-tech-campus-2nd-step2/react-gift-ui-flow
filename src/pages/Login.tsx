7ff671785ecf520602427ce6a289cc988bcdc0ce
JeongSeoyun <xeonj@naver.com>
2024-07-04 23:03:42 +0900

    feat: 로그인페이지 화면 구현

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,21 @@
 //import React from 'react'
+import './Login.css'
+
+import { Button } from "@/components/common/Button"
+import { UnderlineTextField } from "@/components/common/Form/Input/UnderlineTextField"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="Login">
+        <div className="title">Kakao</div>
+        <div className="container">
+            <UnderlineTextField placeholder='이름' className='textFieldId'/>
+            <UnderlineTextField placeholder='비밀번호' className='textFieldPw'/>
+            <Button theme='kakao' size='responsive' className='buttonLogin'>
+                <div>로그인</div>
+            </Button>
+        </div>
+    </div>
   )
 }
 
