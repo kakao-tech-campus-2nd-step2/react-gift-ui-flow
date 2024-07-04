8f54222da5b9079f81bcbaa32ee16e3e8601aacd
Hyoeunkh <lhe4528@knu.ac.kr>
2024-07-05 00:25:55 +0900

    feat: MyAccount 페이지 로그아웃 기능 구현 및 로그인 했을 경우에만 접근 가능하도록 수정

--- src/pages/MyAccountPage.tsx
+++ src/pages/MyAccountPage.tsx
@@ -1,18 +1,31 @@
 /* @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 import { Button } from '@/components/common/Button';
 import { Container } from '@/components/common/Layout/Container';
+import { useAuth } from '@/hooks/useAuth';
 
 export default function MyAccountPage() {
+  const { authToken, setAuthToken } = useAuth();
+  const navigate = useNavigate();
+  if (!authToken) {
+    return <Navigate to="/login" replace={true} />;
+  }
+  const HandleLogout = () => {
+    sessionStorage.clear();
+    setAuthToken(null);
+    navigate('/', { replace: true });
+  };
   return (
     <MyAccountWrapper>
       <Container flexDirection="column" justifyContent="center" alignItems="center" maxWidth="1024px">
-        <Title>userID님 안녕하세요!</Title>
+        <Title>{authToken.id}님 안녕하세요!</Title>
         <Button
           themetype="darkGray"
           size="large"
+          onClick={HandleLogout}
           css={css`
             width: 20%;
             margin-top: 30px;
