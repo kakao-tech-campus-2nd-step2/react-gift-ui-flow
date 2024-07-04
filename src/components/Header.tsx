b09b2caf86857b5f4ce10d39da1519ed8bdbc1a1
yuni <hys990730@gmail.com>
2024-07-04 23:30:15 +0900

    feat: 헤더 컴포넌트 기본 구조 생성

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+const Header: React.FC = () => {
+  return (
+    <header>
+      <h1>My Application</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
