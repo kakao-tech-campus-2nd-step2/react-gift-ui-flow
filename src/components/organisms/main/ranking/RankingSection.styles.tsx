38ae67b13b89c70982b17147a445ce911da52aa5
cla6shade <cla6shade@gmail.com>
2024-07-04 14:01:14 +0900

    design: 랭킹 섹션의 타이틀 컨테이너 분리

--- src/components/organisms/main/ranking/RankingSection.styles.tsx
+++ src/components/organisms/main/ranking/RankingSection.styles.tsx
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 import { BREAKPOINT_SM } from '@styles/size';
 
-// eslint-disable-next-line import/prefer-default-export
 export const RankingSectionTitle = styled.h1`
   font-size: 35px;
   line-height: 50px;
@@ -9,4 +8,9 @@ export const RankingSectionTitle = styled.h1`
   @media (max-width: ${BREAKPOINT_SM}) {
     font-size: 20px;
   }
+`;
+
+export const RankingSectionTitleContainer = styled.div`
+  display: flex;
+  align-items: center;
 `;
