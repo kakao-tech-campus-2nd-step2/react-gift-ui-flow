9b655aa4867e2964fb97c7ad421b726b3deb5b5d
seung365 <bdh3659@naver.com>
2024-07-04 17:11:34 +0900

    feat:ProductList.tsx 구현

---
+++ src/components/Product/ProductList.tsx
@@ -0,0 +1,32 @@
+import { Grid } from '../common/layouts/Grid';
+import { RankingGoodsItems } from '../common/GoodsItem/Ranking';
+
+interface Product {
+  id: number;
+  category: string;
+  name: string;
+}
+
+interface ProductsProps {
+  products: Product[];
+}
+
+const ProductList = ({ products }: ProductsProps) => {
+  return (
+    <Grid columns={{ sm: 3, md: 4, lg: 6 }} gap={20}>
+      {products.map((product, index) => (
+        <div key={product.id}>
+          <RankingGoodsItems
+            imageSrc="/images/gift.jpg"
+            subtitle="BBQ"
+            title="BBQ 양념치킨"
+            amount={29000}
+            rankingIndex={index + 1}
+          />
+        </div>
+      ))}
+    </Grid>
+  );
+};
+
+export default ProductList;
