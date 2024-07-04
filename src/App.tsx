cf1b7efc3f842dc172071755bb5d91d63fc68508
Da-Yeon-Kim <210941@jnu.ac.kr>
2024-07-05 04:08:27 +0900

    feat(Footer): Footer 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Footer } from '@/components/common/Footer/Footer';
 import { Header } from '@/components/common/Header/Header';
 
 const App = () => {
-  return <Header></Header>;
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/common/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      <FooterContent>
+        <FooterText>카카오톡 선물하기</FooterText>
+      </FooterContent>
+    </FooterContainer>
+  );
+};
+
+const FooterContainer = styled.footer``;
+
+const FooterContent = styled.div``;
+
+const FooterText = styled.p``;
