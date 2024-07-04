2ace5a0c8bfbd4724d8f7dfbcd8e34df6a8a16de
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-07-04 16:47:44 +0900

    feat: login페이지에서는 Header, Footer 컴포넌트가 보이지 않게 조건 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import styled from '@emotion/styled';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './routes/Main';
 import ThemePage from './routes/ThemePage';
+import LoginPage from './routes/LoginPage';
 import Footer from './components/Footer';
 
 const App = () => {
   return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+};
+
+const AppContent = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+
+  return (
     <AppContainer>
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/theme/life_small_gift" element={<ThemePage />} />
-        </Routes>
-      </BrowserRouter>
-      <Footer />
+      {!isLoginPage && <Header />}
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/theme/life_small_gift" element={<ThemePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      {!isLoginPage && <Footer />}
     </AppContainer>
   );
 };
 
 export default App;
 
-const AppContainer = styled.div``;
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
