3faa1eb053a727d5b04b27291e33a18f803028b9
dandamdandam <glue0440@gmail.com>
2024-07-04 13:42:19 +0900

    feat: add themeList (mainpage)

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,40 @@
-import { css } from '@emotion/css';
+import { Container } from '@/components/common/layouts/Container';
+import { Grid } from '@/components/common/layouts/Grid';
 
 export default () => {
     return (
-        <div
-            className={css`
-                height: 500px;
-            `}
-        >
-            main
+        <div>
+            {/* theme section */}
+            <section>
+                <Container>
+                    <ThemeList />
+                </Container>
+            </section>
         </div>
+    );
+};
+
+const ThemeList = () => {
+    const themeKey = [
+        '생일',
+        '졸업선물',
+        '스몰럭셔리',
+        '명품선물',
+        '결혼/집들이',
+        '따뜻한선물',
+        '가벼운선물',
+        '팬심저격',
+        '교환권',
+        '건강/비타민',
+        '과일/한우',
+        '출산/키즈',
+    ];
+
+    return (
+        <Grid columns={6}>
+            {themeKey.map((key) => (
+                <div key={key}>{key}</div>
+            ))}
+        </Grid>
     );
 };
