93375fdd1398a274007e1eae164acc3bdde43f33
cla6shade <cla6shade@gmail.com>
2024-07-04 14:29:13 +0900

    refactor: 메인 배너의 스타일 분리

---
+++ src/components/organisms/main/MainBanner.styles.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const BannerTitle = styled.h1`
+  font-size: 28px;
+  padding-left: 16px;
+`;
+
+export const ImageContainer = styled.div`
+  width: 70px;
+  height: 70px;
+`;
--- src/components/organisms/main/MainBanner.tsx
+++ src/components/organisms/main/MainBanner.tsx
@@ -1,8 +1,18 @@
 import Container from '@components/atoms/container/Container';
-import { css } from '@emotion/react';
 import Image from '@components/atoms/image/Image';
-import { MAX_CONTENT_WIDTH } from '@styles/size';
+import { BREAKPOINT_SM, MAX_CONTENT_WIDTH } from '@styles/size';
 import { backgroundColors } from '@styles/colors';
+import {
+  BannerTitle, ImageContainer,
+} from '@components/organisms/main/MainBanner.styles';
+import { css } from '@emotion/react';
+
+const imageStyle = css`
+  border-radius: 24px;
+  @media (max-width: ${BREAKPOINT_SM}) {
+    border-radius: 16px;
+  }
+`;
 
 function MainBanner() {
   return (
@@ -12,20 +22,17 @@ function MainBanner() {
         alignItems="center"
         maxWidth={MAX_CONTENT_WIDTH}
       >
-        <div css={css`
-            width: 70px;
-            height: 70px;
-        `}
-        >
-          <Image ratio="square" radius={25} src="https://picsum.photos/200" />
-        </div>
-        <h1 css={css`
-            font-size: 28px;
-            padding-left: 16px;
-        `}
-        >
+        <ImageContainer>
+          <Image
+            ratio="square"
+            radius={25}
+            src="https://picsum.photos/200"
+            css={imageStyle}
+          />
+        </ImageContainer>
+        <BannerTitle>
           선물 받을 친구를 선택해주세요.
-        </h1>
+        </BannerTitle>
       </Container>
     </Container>
   );
