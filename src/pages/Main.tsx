1395edca6951a3908e62bea22479368dda389dba
JeongSeoyun <xeonj@naver.com>
2024-07-04 14:47:08 +0900

    feat: 메인페이지 헤더 z 인덱스 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -18,31 +18,35 @@ const Main = () => {
         <div className='header'>
             <Header title='선물하기' subtitle='로그인' />
         </div>
-        
-        <div className='selectFriend'>
-            <Image
-                src={choonsik}
-                alt='choonsik'
-                radius={20}
-                ratio={'square'}
-                width={70}
-                height={70}
-             />
-            <div className='selectFriendTitle'>선물 받을 친구를 선택해주세요.</div>
-        </div>
 
-        <CategorySection />
+        <div className='mainContainer'>
 
-        <div className='buttonContainerKakao'>
-            <Button theme='kakao' size='responsive' className='button01'>
-                <div className='buttonSubtitle'>AI가 추천하는 선물</div>
-                <div className='buttonTitle'>선물을 추천받고 싶은 친구를 선택해주세요.</div>
-            </Button>
-        </div>
+            <div className='selectFriend'>
+                <Image
+                    src={choonsik}
+                    alt='choonsik'
+                    radius={20}
+                    ratio={'square'}
+                    width={70}
+                    height={70}
+                />
+                <div className='selectFriendTitle'>선물 받을 친구를 선택해주세요.</div>
+            </div>
 
-        <RankingSection />
+            <CategorySection />
 
-        <Footer title='카카오톡 선물하기'  />
+            <div className='buttonContainerKakao'>
+                <Button theme='kakao' size='responsive' className='button01'>
+                    <div className='buttonSubtitle'>AI가 추천하는 선물</div>
+                    <div className='buttonTitle'>선물을 추천받고 싶은 친구를 선택해주세요.</div>
+                </Button>
+            </div>
+
+            <RankingSection />
+
+            <Footer title='카카오톡 선물하기'  />
+
+        </div> 
     </div>
   )
 }
