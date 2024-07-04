7b51d81f14b4bea2aa4be3aeccfdebb3806409b8
Hyoeunkh <lhe4528@knu.ac.kr>
2024-07-04 15:47:19 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import HomePage from '@/pages/HomePage';
+import { Route, Routes } from 'react-router-dom';
+
+import { MainLayout } from '@/components/feature/Layout/MainLayout';
+import LoginPage from '@/pages/LoginPage';
+import MainPage from '@/pages/MainPage';
+import MyAccountPage from '@/pages/MyAccountPage';
+import ThemePage from '@/pages/ThemePage';
 import { ResetStyles } from '@/styles/reset';
 
 const App = () => {
   return (
     <>
       <ResetStyles />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index path="/" element={<MainPage />} />
+          <Route path="/theme/:themeKey" element={<ThemePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/my-account" element={<MyAccountPage />} />
+        </Route>
+      </Routes>
     </>
   );
 };
