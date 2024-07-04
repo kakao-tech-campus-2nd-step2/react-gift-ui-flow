d2fee426e445c447bb50d10f70fedf439e41eb84
cla6shade <cla6shade@gmail.com>
2024-07-04 13:58:19 +0900

    design: 랭킹 섹션의 제목에 반응형 스타일 추가

--- src/components/organisms/main/ranking/RankingSection.styles.tsx
+++ src/components/organisms/main/ranking/RankingSection.styles.tsx
@@ -1,8 +1,12 @@
 import styled from '@emotion/styled';
+import { BREAKPOINT_SM } from '@styles/size';
 
 // eslint-disable-next-line import/prefer-default-export
 export const RankingSectionTitle = styled.h1`
   font-size: 35px;
   line-height: 50px;
   font-weight: bold;
+  @media (max-width: ${BREAKPOINT_SM}) {
+    font-size: 20px;
+  }
 `;
