1ba0728b0953a70f3118200d6e884d75cd6dcb83
박건규 <geongyu09@gmail.com>
2024-07-04 22:44:14 +0900

    feat: ThemeDetail 페이지 구현

--- src/pages/ThemeDetail.tsx
+++ src/pages/ThemeDetail.tsx
@@ -1,5 +1,38 @@
+import { getThemeDetail } from "@/api";
+import PageLayout from "@/components/common/layouts/PageLayout";
+import { Product } from "@/model/Product";
+import { useEffect, useState } from "react";
+import { Grid } from "../components/common/layouts/Grid/index";
+import { Container } from "@/components/common/layouts/Container";
+import { DefaultGoodsItems } from "@/components/common/GoodsItem/Default";
+import ThemeBanner from "@/components/common/Banner/ThemeBanner";
+import { css } from "@emotion/css";
+
 const ThemeDetail = () => {
-  return <div></div>;
+  const [product, setProduct] = useState<Product[]>([]);
+
+  useEffect(() => {
+    getThemeDetail().then((data) => setProduct(data));
+  }, []);
+
+  return (
+    <PageLayout>
+      <ThemeBanner />
+      <div
+        className={css`
+          margin-top: 2rem;
+        `}
+      >
+        <Container>
+          <Grid columns={4} gap={32}>
+            {product?.map((product) => (
+              <DefaultGoodsItems key={product.title} {...product} />
+            ))}
+          </Grid>
+        </Container>
+      </div>
+    </PageLayout>
+  );
 };
 
 export default ThemeDetail;
