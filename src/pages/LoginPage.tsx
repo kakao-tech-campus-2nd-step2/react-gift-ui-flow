b8cc5ea7202194098990342568c4090ad4cd6016
yuni <hys990730@gmail.com>
2024-07-05 01:08:18 +0900

    feat: MyAccountPage에 로그아웃 처리 로직 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const MyAccountPage: React.FC = () => {
+  const handleLogout = () => {
+    alert('Logged out');
+  };
+
   return (
     <div>
       <h1>My Account</h1>
-      <button>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
