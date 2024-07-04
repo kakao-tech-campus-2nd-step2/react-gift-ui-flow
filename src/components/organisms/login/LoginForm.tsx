438f41b14dfccc4c315b4712cde4738d36eded93
cla6shade <cla6shade@gmail.com>
2024-07-04 14:34:55 +0900

    design: loginForm이 화면 중앙에서 일정한 padding을 가지도록 수정

--- src/components/organisms/login/LoginForm.tsx
+++ src/components/organisms/login/LoginForm.tsx
@@ -19,48 +19,54 @@ function LoginForm() {
   return (
     <Container
       elementSize="full-width"
-      cssProps={{
-        border: '1px solid rgba(0, 0, 0, 0.12)',
-      }}
-      maxWidth="580px"
+      justifyContent="center"
+      padding="20px"
     >
       <Container
+        cssProps={{
+          border: '1px solid rgba(0, 0, 0, 0.12)',
+        }}
+        maxWidth="580px"
         elementSize="full-width"
-        flexDirection="column"
-        padding="60px 52px"
       >
-        <Input
-          elementSize={{
-            width: '100%',
-            height: '46px',
-          }}
-          placeholder="이름"
-        />
-        <div css={css`
+        <Container
+          elementSize="full-width"
+          flexDirection="column"
+          padding="60px 52px"
+        >
+          <Input
+            elementSize={{
+              width: '100%',
+              height: '46px',
+            }}
+            placeholder="이름"
+          />
+          <div css={css`
             height: 16px;
         `}
-        />
-        <Input
-          elementSize={{
-            width: '100%',
-            height: '46px',
-          }}
-          placeholder="비밀번호"
-          type="password"
-        />
-        <div css={css`
+          />
+          <Input
+            elementSize={{
+              width: '100%',
+              height: '46px',
+            }}
+            placeholder="비밀번호"
+            type="password"
+          />
+          <div css={css`
             height: 60px;
         `}
-        />
-        <Button
-          theme="kakao"
-          elementSize={{
-            width: '100%',
-            height: '60px',
-          }}
-          text="로그인"
-          onClick={onLoginClick}
-        />
+          />
+          <Button
+            theme="kakao"
+            elementSize={{
+              width: '100%',
+              height: '60px',
+            }}
+            text="로그인"
+            onClick={onLoginClick}
+          />
+        </Container>
       </Container>
     </Container>
   );
