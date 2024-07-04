1e85e1b0fa2692ea224494965b8510d0651e6bb5
loveydev <tjgus9650@gmail.com>
2024-07-04 16:31:02 +0900

    Refactor: improve code readability and structure

--- src/components/common/layouts/Footer/Footer.tsx
+++ src/components/common/layouts/Footer/Footer.tsx
@@ -1,10 +1,22 @@
 import styled from '@emotion/styled'
 
+const Footer: React.FC = () => {
+  return (
+    <Wrapper>
+      <FooterContainer>
+        <FooterInner className='inner'>
+          <FooterLogo src="https://img1.kakaocdn.net/thumb/C140x20@2x.fwebp.q82/?fname=https%3A%2F%2Fgift-s.kakaocdn.net%2Fdn%2Fgift%2Fimages%2Fm640%2Fpc_smallbi_201223.png" alt="카카오톡 선물하기 로고2" />
+        </FooterInner>
+      </FooterContainer>
+    </Wrapper>
+  )
+}
+
+export default Footer
+
 const Wrapper = styled.footer`
   width: 100%;
   max-width: 100vw;
-  position: fixed;
-  bottom: 0;
   box-sizing: border-box;
   padding: 28px 16px 88px;
   background-color: rgb(250, 250, 252);
@@ -12,37 +24,23 @@ const Wrapper = styled.footer`
   @media screen and (min-width: 768px) {
     padding: 40px 16px 120px;
   }
-
-  div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    .inner {
-      width: 100%;
-      max-width: 1024px;
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
-      align-items: flex-start;
-
-      img {
-        height: 20px;
-      }
-    }
-  }
 `;
 
-const Footer = () => {
-  return (
-    <Wrapper>
-      <div>
-        <div className='inner'>
-          <img src="https://img1.kakaocdn.net/thumb/C140x20@2x.fwebp.q82/?fname=https%3A%2F%2Fgift-s.kakaocdn.net%2Fdn%2Fgift%2Fimages%2Fm640%2Fpc_smallbi_201223.png" alt="카카오톡 선물하기 로고2" />
-        </div>
-      </div>
-    </Wrapper>
-  )
-}
+const FooterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-export default Footer
+const FooterInner = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;  
+`;
+
+const FooterLogo = styled.img`
+  height: 20px;
+`;
--- src/components/common/layouts/Header/Header.tsx
+++ src/components/common/layouts/Header/Header.tsx
@@ -1,5 +1,22 @@
 import styled from '@emotion/styled'
 
+const Header: React.FC = () => {
+  return (
+    <Wrapper>
+      <HeaderContainer>
+        <HeaderNavigator className="inner">
+          <a href="/">
+            <HeaderLogo src="https://gift-s.kakaocdn.net/dn/gift/images/m640/pc_gift_logo.png" alt="카카오 선물하기 로고" />
+          </a>
+          <LoginText>로그인</LoginText>
+        </HeaderNavigator>
+      </HeaderContainer>
+    </Wrapper>
+  )
+}
+
+export default Header
+
 const Wrapper = styled.header`
   position: fixed;
   box-sizing: border-box;
@@ -9,48 +26,31 @@ const Wrapper = styled.header`
   height: 54px;
   background-color: rgb(255, 255, 255);
   padding: 0px 16px;
-  
-  div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    .inner {
-      width: 100%;
-      max-width: 1024px;
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: center;
-
-      img {
-        height: 54px;
-      }
-
-      p {
-        align-items: center;
-        font-size: 14px;
-        color: rgb(0, 0, 0);
-        text-decoration: none;
-        cursor: pointer;
-      }
-    }
-  }
 `;
 
-const Header = () => {
-  return (
-    <Wrapper>
-      <div>
-        <div className="inner">
-          <a href="/">
-            <img src="https://gift-s.kakaocdn.net/dn/gift/images/m640/pc_gift_logo.png" alt="카카오 선물하기 로고" />
-          </a>
-          <p>로그인</p>
-        </div>
-      </div>
-    </Wrapper>
-  )
-}
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-export default Header
+const HeaderNavigator = styled.div`
+  width: 100%;
+  max-width: 1024px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const HeaderLogo = styled.img`
+  height: 54px;
+`;
+
+const LoginText = styled.p`
+  align-items: center;
+  font-size: 14px;
+  color: rgb(0, 0, 0);
+  text-decoration: none;
+  cursor: pointer;
+`;
