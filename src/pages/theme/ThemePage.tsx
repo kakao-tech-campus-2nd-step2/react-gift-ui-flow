7f344ea47a57253e43fade213bd01b0270f69dfa
Dobbymin <gmin0701@knu.ac.kr>
2024-07-04 16:52:23 +0900

    feat(theme): ThemePage ui 구현 및 컴포넌트 분리

---
+++ src/pages/theme/ThemePage.tsx
@@ -0,0 +1,13 @@
+import GiftSection from './components/GiftSection';
+import TitleSection from './components/TitleSection';
+
+const ThemePage = () => {
+  return (
+    <>
+      <TitleSection />
+      <GiftSection />
+    </>
+  );
+};
+
+export default ThemePage;
