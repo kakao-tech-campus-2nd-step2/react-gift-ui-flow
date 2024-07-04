4e83c87c07c78573c246b28990a6a0bd0ed93a65
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-07-04 14:46:58 +0900

    feat: Theme 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './routes/Main';
+import ThemePage from './routes/ThemePage';
 import Footer from './components/Footer';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/theme/life_small_gift" element={<ThemePage />} />
         </Routes>
       </BrowserRouter>
       <Footer />
