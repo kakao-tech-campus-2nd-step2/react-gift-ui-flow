1ad6d99962bdee95f2c142d59a4f9d5022a12c10
박건규 <geongyu09@gmail.com>
2024-07-04 22:45:43 +0900

    feat: detail 페이지에서의 api 요청문 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,3 +7,9 @@ export const getRealtimeRank = async (
   const fetchUrl = isExpanded ? API.REALTIMERANK : API.REALTIEMRANK_PREVIEW;
   return await fetch(fetchUrl).then((res) => res.json());
 };
+
+export const getThemeDetail =
+  async () // themeKey: string, : 원래대로라면 themeKey를 받아와야 하지만, 지금은 사용하지 않으므로 제거
+  : Promise<RankingItem[]> => {
+    return await fetch(API.THEME_DETAIL).then((res) => res.json());
+  };
--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,7 @@
 const API = {
   REALTIMERANK: "/api/real-time-ranking.json",
   REALTIEMRANK_PREVIEW: "/api/real-time-ranking-preview.json",
+  THEME_DETAIL: "/api/theme-product.json",
 };
 
 Object.freeze(API);
