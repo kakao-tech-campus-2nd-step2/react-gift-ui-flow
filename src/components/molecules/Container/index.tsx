d19098c4491a79768e769b7da8e112344ff01948
JaeAnHan <jaean1999@naver.com>
2024-07-04 16:40:56 +0900

    chore: 프롭스 이름 변경

--- src/components/molecules/Container/index.tsx
+++ src/components/molecules/Container/index.tsx
@@ -1,11 +1,11 @@
 import styled from '@emotion/styled';
 import { forwardRef } from 'react';
 
-import type { Props } from '@/components/atoms/FlatFlex';
+import type { FlatFlexProps } from '@/components/atoms/FlatFlex';
 import { FlatFlex } from '@/components/atoms/FlatFlex';
 
-export const Container: React.FC<Props> = forwardRef(
-  ({ children, ...props }: Props, ref: React.Ref<HTMLDivElement>) => {
+export const Container: React.FC<FlatFlexProps> = forwardRef(
+  ({ children, ...props }: FlatFlexProps, ref: React.Ref<HTMLDivElement>) => {
     return (
       <Wrapper ref={ref} {...props}>
         <FlatFlex {...props}>{children}</FlatFlex>
