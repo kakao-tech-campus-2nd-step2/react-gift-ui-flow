ae962a3121a7ecdac1c82154e1191977c68ddf70
Dobbymin <gmin0701@knu.ac.kr>
2024-07-05 01:47:02 +0900

    feat(login): 로그인 시 이름 session storage에 저장 기능 구현

--- src/pages/login/components/LoginSection.tsx
+++ src/pages/login/components/LoginSection.tsx
@@ -1,14 +1,28 @@
 import { Button } from '@/components/common/Button';
 import * as styles from '../styles';
+import { useNavigate } from 'react-router-dom';
+import { PATHS } from '@/constants/Path';
+import { useState } from 'react';
 
 const LoginSection = () => {
+  const [userId, setUserId] = useState<string>('');
+  const navigate = useNavigate();
+
+  const HandlerChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+  };
+
+  const HandlerLogin = () => {
+    sessionStorage.setItem('authToken', userId);
+    navigate(PATHS.MAIN);
+  };
   return (
     <styles.SectionLayout>
-      <styles.LoginInput placeholder="이름" type="text" />
+      <styles.LoginInput placeholder="이름" value={userId} onChange={HandlerChange} type="text" />
       <styles.InputSpacer></styles.InputSpacer>
       <styles.LoginInput placeholder="비밀번호" type="password" />
       <styles.ButtonSpacer></styles.ButtonSpacer>
-      <Button>로그인</Button>
+      <Button onClick={HandlerLogin}>로그인</Button>
     </styles.SectionLayout>
   );
 };
