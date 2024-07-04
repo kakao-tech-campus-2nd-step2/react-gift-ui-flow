f8678a71251f004e7535fc5a80fe6c5982b6e9c8
JaeAnHan <jaean1999@naver.com>
2024-07-04 16:41:44 +0900

    feat: FlexItemsBox 프롭스에 클래스 네임 추가

--- src/components/molecules/FlexItemsBox/index.tsx
+++ src/components/molecules/FlexItemsBox/index.tsx
@@ -6,14 +6,16 @@ import { FlatFlex } from '@/components/atoms/FlatFlex';
 type FlexItemsBoxProp<T> = {
   flexItems: T[];
   FlexItemComponent: ElementType;
+  className?: string;
 };
 
 export const FlexItemsBox = <T,>({
   flexItems,
   FlexItemComponent,
+  className,
 }: FlexItemsBoxProp<T>): ReactElement => {
   return (
-    <FlatFlex>
+    <FlatFlex className={className}>
       {flexItems.map((props) => (
         <FlexItemComponent {...props} />
       ))}
