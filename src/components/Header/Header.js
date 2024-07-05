2128d60130d08c9b02e9e94939c7094eb967c3e4
yunseong0404 <333665@naver.com>
2024-07-06 03:35:11 +0900

    fix: Header.js Link 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,9 @@
 import '@/components/Header/header.css';
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 
 export default function Header() {
@@ -8,8 +11,12 @@ export default function Header() {
     <div>
       <header className='header-container'>
         <div className='header-item'>
+          <Link to='/' className='link'>
             <div className='header-item__gift'>선물하기</div>
+          </Link>
+          <Link to='/login' className='link'>
             <div className='header-item__login'>로그인</div>
+          </Link>
         </div>
       </header>
     </div>
