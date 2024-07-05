f7e1581b0994f7aba19435e138507ce29fe6be23
KimKyuHoi <k546kh@gmail.com>
2024-07-05 14:49:49 +0900

    Feat: RouterProvider 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 
+import { AuthProvider } from '@/contexts/AuthContext';
+
 import { router } from './Router';
 
 const App = () => {
   return (
-    <>
+    <AuthProvider>
       <RouterProvider router={router} />
-    </>
+    </AuthProvider>
   );
 };
 
