8f751418243eea0f272ac29c69d05f072c718679
Kuheewon <selly3109@naver.com>
2024-07-05 17:21:21 +0900

    refactor: 재사용성을 고려한 Header

--- src/pages/ThemePage.tsx
+++ src/pages/ThemePage.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 
 import ThemeHeader from '../components/common/Header/ThemeHeader';
 
 const ThemePage: React.FC = () => {
-  const { themeKey } = useParams<{ themeKey: string }>();
+  const title = '가벼운 선물';
+  const subtitle = ['예산은 가볍게, 감동은 무겁게 ❤️', '당신의 센스를 뽐내줄 부담 없는 선물'];
 
   return (
     <div>
-      <ThemeHeader />
-      <p>This is the theme page for: {themeKey}</p>
+      <ThemeHeader title={title} subtitle={subtitle} />
     </div>
   );
 };
