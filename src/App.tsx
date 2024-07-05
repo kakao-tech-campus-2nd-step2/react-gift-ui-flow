c8ea2b723d3525c39d201f13d3adcd46aa07560a
seongikx <tjddlr3512@pusan.ac.kr>
2024-07-05 23:21:04 +0900

    Feat: React-Router-Dom 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import styled from '@emotion/styled';
+import React from 'react';
+import { BrowserRouter as Router, Navigate,Route, Routes } from 'react-router-dom';
 
-const App = () => {
-  const name = 'Josh Perez';
+import { AuthProvider, useAuth } from './contexts/AuthContext';
+import LoginPage from './pages/loginpage';
+import MainPage from './pages/mainpage';
+import MyPage from './pages/mypage';
 
-  return (
-    <div>
-      <Title>Hello, {name}</Title>
-    </div>
-  );
+const App: React.FC = () => (
+  <Router>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/my-account" element={<PrivateRoute component={MyPage} />} />
+      </Routes>
+    </AuthProvider>
+  </Router>
+);
+
+const PrivateRoute: React.FC<{ component: React.ComponentType }> = ({ component: Component }) => {
+  const { user } = useAuth();
+  return user ? <Component /> : <Navigate to="/login" />;
 };
 
 export default App;
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  color: gray;
-`;
