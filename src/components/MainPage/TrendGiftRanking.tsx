0672cc2b4d4fdf4a3649dcebcd0e4580b2312311
wjdsbs <wjdsbss@gmail.com>
2024-07-05 20:49:26 +0900

    feat: 실시간 급상승 선물랭킹 필터 기능 구현

--- src/components/MainPage/TrendGiftRanking.tsx
+++ src/components/MainPage/TrendGiftRanking.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { useState } from 'react';
 
 import { Container } from '../common/layouts/Container';
 import { Grid } from '../common/layouts/Grid';
@@ -15,11 +16,6 @@ const Title = styled.h2`
 
 const FiltersContainer = styled.div``;
 
-const FilterItem = styled.div`
-  margin: 0 auto;
-  text-align: center;
-  max-width: 100%;
-`;
 const Wrapper = styled(Grid)`
   padding: 20px 0px 7px;
   max-width: 100%;
@@ -27,9 +23,7 @@ const Wrapper = styled(Grid)`
 
 const Icon = styled.div`
   display: flex;
-  -webkit-box-pack: center;
   justify-content: center;
-  -webkit-box-align: center;
   align-items: center;
   width: 60px;
   height: 60px;
@@ -39,7 +33,26 @@ const Icon = styled.div`
   font-weight: 700;
   background-color: rgb(230, 241, 255);
   transition: background-color 200ms ease 0s;
+`;
+
+const FilterItem = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  max-width: 100%;
   cursor: pointer;
+
+  &.active {
+    p {
+      color: rgb(70, 132, 233);
+      font-weight: 700;
+    }
+
+    div {
+      background-color: rgb(70, 132, 233);
+      font-weight: 700;
+      color: white;
+    }
+  }
 `;
 
 const FilterType = styled.div`
@@ -57,28 +70,61 @@ const FilterType = styled.div`
     color: rgba(70, 132, 233, 0.7);
     font-weight: 400;
     font-size: 22px;
+    cursor: pointer;
+    outline: none;
+    border: none;
+    background: none;
+    transition: color 0.2s ease-in-out;
+  }
+
+  button.active {
+    color: rgba(70, 132, 233, 1);
+    font-weight: 700;
   }
 `;
 
 const TrendGiftRanking = () => {
+  const [genderFilter, setGenderFilter] = useState('ALL');
+  const [rankingFilter, setRankingFilter] = useState('RECEIVING');
+
+  const handleGenderFilter = (filter: string) => {
+    setGenderFilter(filter);
+  };
+
+  const handleRankingFilter = (filter: string) => {
+    setRankingFilter(filter);
+  };
+
   return (
     <Container>
       <Title>실시간 급상승 선물 랭킹</Title>
       <FiltersContainer>
         <Wrapper columns={4} gap={12}>
-          <FilterItem>
+          <FilterItem
+            className={genderFilter === 'ALL' ? 'active' : ''}
+            onClick={() => handleGenderFilter('ALL')}
+          >
             <Icon>ALL</Icon>
             <p>전체</p>
           </FilterItem>
-          <FilterItem>
+          <FilterItem
+            className={genderFilter === 'FEMALE' ? 'active' : ''}
+            onClick={() => handleGenderFilter('FEMALE')}
+          >
             <Icon>👩🏻‍🦳</Icon>
             <p>여성이</p>
           </FilterItem>
-          <FilterItem>
+          <FilterItem
+            className={genderFilter === 'MALE' ? 'active' : ''}
+            onClick={() => handleGenderFilter('MALE')}
+          >
             <Icon>👨🏻‍🦳</Icon>
             <p>남성이</p>
           </FilterItem>
-          <FilterItem>
+          <FilterItem
+            className={genderFilter === 'TEENAGER' ? 'active' : ''}
+            onClick={() => handleGenderFilter('TEENAGER')}
+          >
             <Icon>👦🏻</Icon>
             <p>청소년이</p>
           </FilterItem>
@@ -86,9 +132,24 @@ const TrendGiftRanking = () => {
 
         <div style={{ height: '16px' }}></div>
         <FilterType>
-          <button>받고 싶어한</button>
-          <button>많이 선물한</button>
-          <button>위시로 받은</button>
+          <button
+            className={rankingFilter === 'RECEIVING' ? 'active' : ''}
+            onClick={() => handleRankingFilter('RECEIVING')}
+          >
+            받고 싶어한
+          </button>
+          <button
+            className={rankingFilter === 'SENDING' ? 'active' : ''}
+            onClick={() => handleRankingFilter('SENDING')}
+          >
+            많이 선물한
+          </button>
+          <button
+            className={rankingFilter === 'WISH' ? 'active' : ''}
+            onClick={() => handleRankingFilter('WISH')}
+          >
+            위시로 받은
+          </button>
         </FilterType>
         <TrendGiftRankingGrid></TrendGiftRankingGrid>
       </FiltersContainer>
