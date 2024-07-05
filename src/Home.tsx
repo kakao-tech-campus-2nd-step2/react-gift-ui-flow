9adee3639e00d39afb4b84c73c78dd0dbd03fd4e
HyeonChae1104 <211063@jnu.ac.kr>
2024-07-06 00:19:38 +0900

    feat: yellow btn 추가

--- src/Home.tsx
+++ src/Home.tsx
@@ -21,6 +21,10 @@ const Home: React.FC = () => {
                     ))}
                 </CategoryList>
             </CategorySection>
+            <YellowButton>
+                <ButtonTitle>AI가 추천하는 선물</ButtonTitle>
+                <ButtonSubtitle>선물을 추천받고 싶은 친구를 선택해주세요.</ButtonSubtitle>
+            </YellowButton>
         </MainContainer>
     );
 };
@@ -88,4 +92,27 @@ const CategoryLabel = styled.span`
     font-size: 14px;
     color: #000;
     text-align: center;
+`;
+
+const YellowButton = styled.button`
+    background-color: yellow;
+    padding: 20px;
+    text-align: center;
+    border: none;
+    cursor: pointer;
+    margin-top: 20px;
+    width: 100%;
+    max-width: 1200px;
+`;
+
+const ButtonTitle = styled.h2`
+    font-size: 18px;
+    color: #000;
+    margin: 0;
+`;
+
+const ButtonSubtitle = styled.p`
+    font-size: 14px;
+    color: #000;
+    margin: 5px 0 0 0;
 `;
