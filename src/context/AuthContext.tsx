db157bac1ce756ec9c245758da98ed962e4af3d7
yuni <hys990730@gmail.com>
2024-07-05 18:54:57 +0900

    feat: create AuthContext for managing authToken

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,25 @@
+import type { ReactNode } from 'react';
+import React, { createContext, useContext, useState } from 'react';
+
+interface AuthContextType {
+  authToken: string | null;
+  setAuthToken: (token: string | null) => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [authToken, setAuthToken] = useState<string | null>(sessionStorage.getItem('authToken'));
+
+  return (
+    <AuthContext.Provider value={{ authToken, setAuthToken }}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
