9b5537d46fdea14930e31036bae7224dd68bcbfa
loveydev <tjgus9650@gmail.com>
2024-07-05 13:53:20 +0900

    Feat: 테마 페이지 및 라우팅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import LoginPage from "@/pages/LoginPage";
 import MainPage from "@/pages/MainPage";
+import MyAccountPage from "@/pages/MyAccountPage";
 import ThemePage from "@/pages/ThemePage";
 
 const App = () => {
@@ -9,6 +11,8 @@ const App = () => {
         <Routes>
           <Route path="/" Component={MainPage} />
           <Route path="/theme/:themeKey" Component={ThemePage} />
+          <Route path="/login" Component={LoginPage} />
+          <Route path="/my-account" Component={MyAccountPage} />
         </Routes>
       </Router>
   );
