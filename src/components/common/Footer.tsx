86da641781e147347ef26a0824b7618d1f951a17
HyeonChae1104 <211063@jnu.ac.kr>
2024-07-05 13:04:00 +0900

    feat: 공통 footer 추가

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,13 @@
+import './Footer.css'; // 스타일링을 위한 CSS 파일
+
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer">
+            <p>© 2024 Gift Ranking App</p>
+        </footer>
+    );
+};
+
+export default Footer;
