52a094be90c17a630e23c19dd2ebb794f03d874e
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-07-05 15:27:21 +0900

    feat : AuthContext 전역적으로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,28 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
+import MyPage from './components/pages/MyPage';
 import ThemePage from './components/pages/ThemePage';
+import { AuthProvider } from './contexts/AuthContext';
 
 const App = () => {
   return (
-    <Router>
-      <div className="app">
-        <Header />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/theme/:themeKey" element={<ThemePage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="app">
+          <Header />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/theme/:themeKey" element={<ThemePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/my-account" element={<MyPage />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 };
 
