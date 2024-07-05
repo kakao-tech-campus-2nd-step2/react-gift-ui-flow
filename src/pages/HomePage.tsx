eae5f40e843528d0d26b1d38466efd2d9758ea16
Kuheewon <selly3109@naver.com>
2024-07-05 14:26:14 +0900

    feat. 상품 목록 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -76,6 +76,79 @@ const categories = [
   },
 ];
 
+const products = [
+  {
+    id: 1,
+    name: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: 29000,
+    image:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+  },
+  {
+    id: 2,
+    name: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: 29000,
+    image:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+  },
+  {
+    id: 3,
+    name: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: 29000,
+    image:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+  },
+  {
+    id: 4,
+    name: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: 29000,
+    image:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+  },
+  {
+    id: 5,
+    name: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: 29000,
+    image:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+  },
+  {
+    id: 6,
+    name: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: 29000,
+    image:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+  },
+  {
+    id: 7,
+    name: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: 29000,
+    image:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+  },
+  {
+    id: 8,
+    name: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: 29000,
+    image:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+  },
+  {
+    id: 9,
+    name: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: 29000,
+    image:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+  },
+  {
+    id: 10,
+    name: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    price: 29000,
+    image:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+  },
+];
+
 const containerStyle: React.CSSProperties = {
   padding: '20px',
   display: 'flex',
@@ -163,6 +236,50 @@ const filterContainerStyle: React.CSSProperties = {
   width: '100%',
   height: '16px',
   margin: '0 40px',
+};
+
+const productCardStyle: React.CSSProperties = {
+  border: 'none',
+  overflow: 'hidden',
+  textAlign: 'center',
+  width: '200px',
+};
+
+const productImageStyle: React.CSSProperties = {
+  width: '100%',
+  borderRadius: '10px',
+  height: 'auto',
+};
+
+const productInfoStyle: React.CSSProperties = {
+  padding: '10px',
+};
+
+const productNameStyle: React.CSSProperties = {
+  fontSize: '14px',
+  fontWeight: 'bold',
+};
+
+const productPriceStyle: React.CSSProperties = {
+  fontSize: '16px',
+  color: '#000',
+  marginTop: '5px',
+};
+
+const ProductList: React.FC = () => {
+  return (
+    <div style={containerStyle}>
+      {products.map((product) => (
+        <div key={product.id} style={productCardStyle}>
+          <img src={product.image} alt={product.name} style={productImageStyle} />
+          <div style={productInfoStyle}>
+            <div style={productNameStyle}>{product.name}</div>
+            <div style={productPriceStyle}>{product.price}원</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 const HomePage: React.FC = () => {
@@ -276,6 +393,9 @@ const HomePage: React.FC = () => {
               </button>
             </div>
           </div>
+        </div>
+        <div>
+          <ProductList />
         </div>
       </section>
     </div>
