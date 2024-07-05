e5a8860290c70f4e49256be149f8a5d8585e2859
yunseong0404 <333665@naver.com>
2024-07-05 21:37:07 +0900

    feat: TargetCategory 구현

---
+++ src/components/common/Theme/TargetCategory.js
@@ -0,0 +1,16 @@
+import './TargetCategory.css';
+
+import React from 'react'
+
+
+export default function TargetCategory({ icon, target, isSelected, onClick }) {
+  return (
+    <div
+        className='targetCategory-container'
+        onClick={onClick}
+    >
+      <div className={`${isSelected ? 'icon_selected' : 'targetCategory__icon'}`}>{icon}</div>
+      <div className={`${isSelected ? 'target_selected' : 'targetCategory__target'}`}>{target}</div>
+    </div>
+  )
+}
