9b5f92a57904fb82ac0c42a69193577724f29178
JaeAnHan <jaean1999@naver.com>
2024-07-05 18:05:06 +0900

    feat: ThemeItems 라벨 예시 페이지처럼 하나씩 명시

--- src/components/organisms/ThemeCategories/index.tsx
+++ src/components/organisms/ThemeCategories/index.tsx
@@ -9,6 +9,21 @@ const columns = {
   md: 6,
 };
 
+const labels = [
+  '생일',
+  '졸업선물',
+  '스몰럭셔리',
+  '명품선물',
+  '결혼/집들이',
+  '따듯한선물',
+  '가벼운선물',
+  '팬심저격',
+  '교환권',
+  '건강/비타민',
+  '과일/한우',
+  '출산/키즈',
+];
+
 const themeItems: ThemeItemProp[] = [];
 
 for (let i = 0; i < 12; i++) {
@@ -16,7 +31,7 @@ for (let i = 0; i < 12; i++) {
     id: i,
     imgSrc:
       'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
-    pContent: 'pContent',
+    pContent: labels[i],
     linkTo: 'theme/life_small_gift',
   });
 }
