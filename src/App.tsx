24edc7d0f51206b0a7866a2d1cd4896fe249e638
Eric <real.dassy@gmail.com>
2024-07-06 00:59:04 +0900

    [feat] App 컴포넌트 구성 및 라우팅 설정
    - Global 컴포넌트와 emotionReset을 사용하여 기본 CSS 리셋 적용
    - BrowserRouter를 사용하여 라우팅 설정
    - Header, Footer 컴포넌트 추가하여 전체 레이아웃 구성
    - Routes를 사용하여 Login, LifeSmallGift, Main 페이지 라우팅 설정
    - main 태그에 minHeight 스타일을 적용하여 Footer가 항상 하단에 위치하도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import styled from '@emotion/styled';
+import { Global } from '@emotion/react'
+import emotionReset from 'emotion-reset'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Button } from '@components/common/Button/Button'
+import { Header } from './presentation/components/Header'
+import { Login } from './presentation/pages/Login'
+import { Footer } from './presentation/components/Footer'
+import { Main } from '@pages/Main/Main'
+import { LifeSmallGift } from '@pages/LifeSmallGift'
 
-const App = () => {
-  const name = 'Josh Perez';
+function App() {
+	return (
+		<>
+			<Global styles={emotionReset} />
+			<Router>
+				<Header />
+				<main style={{ minHeight: '100vh' }}>
+					<Routes>
+						<Route path="/login" element={<Login />} />
+						<Route path="/theme/life_small_gift" element={<LifeSmallGift />} />
+						<Route path="/" element={<Main />} />
+					</Routes>
+				</main>
+				<Footer />
+			</Router>
+		</>
+	)
+}
 
-  return (
-    <div>
-      <Title>Hello, {name}</Title>
-    </div>
-  );
-};
-
-export default App;
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  color: gray;
-`;
+export default App
