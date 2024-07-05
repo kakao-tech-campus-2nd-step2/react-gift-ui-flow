f74099afcfba333f7586db2c1518f6828e191058
daeun0726 <delee2008@naver.com>
2024-07-05 21:39:55 +0900

    feat : main 페이지 구현

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 - [x] Footer 컴포넌트 생성
 - [x] Header 컴포넌트 생성
-- [ ] Url Path 별 페이지 생성- 메인 페이지 (**`/`**)
+- [x] Url Path 별 페이지 생성- 메인 페이지 (**`/`**)
 - [x] Url Path 별 페이지 생성 - Theme 페이지(**`/theme/:themeKey`**)
 - [x] Url Path 별 페이지 생성 - 로그인 페이지(**`/login`**)
 - [x] Url Path 별 페이지 생성 - 나의 페이지(**`/my-account`**)
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Footer } from '@/components/common/layouts/Footer';
 import { Header } from '@/components/common/layouts/Header';
-import { MyAccount } from '@/pages/MyAccount';
+import { Home } from '@/pages/Home';
 
 const App = () => {
   return (
     <div>
       <Header></Header>
-      <MyAccount name="kakao"></MyAccount>
+      <Home />
       <Footer>카카오톡 선물하기</Footer>
     </div>
   );
