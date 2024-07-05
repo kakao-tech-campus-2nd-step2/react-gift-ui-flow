f89a6a3e88b31b649e809d18d95b8aca0199c9af
userjmmm <a@as-MacBook-Pro.local>
2024-07-05 17:49:51 +0900

    feat: Theme 페이지 라우팅 추가

--- gift-app/src/App.tsx
+++ gift-app/src/App.tsx
@@ -1,12 +1,14 @@
 import '@styles/reset.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main'
+import Theme from './pages/Theme';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/theme" element={<Theme />} />
       </Routes>
     </Router>
   );
---
+++ gift-app/src/pages/Theme.tsx
@@ -0,0 +1,11 @@
+import Layout from '@components/Layout/Layout';
+
+const Theme = () => {
+  return (
+    <Layout>
+      <div>확인용</div>
+    </Layout>
+  );
+};
+
+export default Theme;
