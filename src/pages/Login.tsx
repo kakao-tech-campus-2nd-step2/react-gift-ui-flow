5ae4035a8f51add1950615e1d593295cc148629a
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2024-07-05 23:17:49 +0900

    feat : login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,17 @@
+import '@styles/login.css';
+
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login">
+      <img
+        className="login__title"
+        src="https://kakao-tech-week2-kakao-gift-ui-flow.pages.dev/static/media/kakao_logo.60e99555125ae4c4b8f23f6c5faee9ec.svg"
+      />
+      <div className="login__form">
+        <input placeholder="이름" />
+        <input placeholder="비밀번호" />
+        <button className="login__button">로그인</button>
+      </div>
     </div>
   );
 }
