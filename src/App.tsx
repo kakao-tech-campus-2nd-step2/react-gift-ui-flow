8cccf594f61d80b9592a8913bcca0e08cbbc741a
Dobbymin <gmin0701@knu.ac.kr>
2024-07-05 21:05:40 +0900

    feat(App.tsx): AuthProvider로 페이지 전체 로그인 여부 판단

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 
 import { router } from './router';
+import { AuthProvider } from './utils/AuthContext';
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />;
+    </AuthProvider>
+  );
 };
 
 export default App;
