0e47ea212187111bb0435f0ebaecb917e0563168
Eric <real.dassy@gmail.com>
2024-07-06 00:58:28 +0900

    [feat] AuthProvider 최상단에 추가하여 인증 상태 관리
    - AuthProvider를 최상단에 추가하여 애플리케이션 전반에서 인증 상태를 관리할 수 있도록 설정
    - 모든 컴포넌트에서 AuthContext를 사용하여 로그인 상태를 쉽게 접근 및 조작 가능

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import '@/styles';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { AuthProvider } from './shared/utils/AuthContext'
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import App from '@/App';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+	<React.StrictMode>
+		<AuthProvider>
+			<App />
+		</AuthProvider>
+	</React.StrictMode>,
+)
