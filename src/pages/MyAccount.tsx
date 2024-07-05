05e46cf5569b8f6a9cedb51ec281a040759e1991
Jisu Lee <dlwltn0430@naver.com>
2024-07-05 22:35:24 +0900

    feat: my account 화면 구현

--- src/pages/MyAccount.tsx
+++ src/pages/MyAccount.tsx
@@ -1,5 +1,33 @@
-function MyAccount() {
-  return <div></div>;
-}
+import styled from '@emotion/styled';
+import React from 'react';
+
+import { Button } from '@/components/common/Button';
+// import { useAuth } from '@/context/AuthContext';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const WelcomeMessage = styled.h1`
+  font-size: 24px;
+  padding-bottom: 30px;
+`;
+
+const MyAccount: React.FC = () => {
+  //  const { logout } = useAuth();
+
+  return (
+    <Container>
+      <WelcomeMessage>{}님 안녕하세요!</WelcomeMessage>
+      <Button theme="darkGray" size="small" style={{ width: '200px' }}>
+        로그아웃
+      </Button>
+    </Container>
+  );
+};
 
 export default MyAccount;
