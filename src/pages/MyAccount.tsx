d49c9f676a6c4b2b4007da123f3a8643f61fca47
mingkyeongg <ming0820@naver.com>
2024-07-05 12:55:25 +0900

    feat: create my-account page

--- src/pages/MyAccount.tsx
+++ src/pages/MyAccount.tsx
@@ -1,8 +1,27 @@
+import styled from "@emotion/styled";
+
+import { Button } from "@/components/common/Button";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const StyledText = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  margin-bottom: 10px;
+`;
+
 const MyAccount = () => {
 	  return (
-	<div>
-	  <h1>My Account</h1>
-	</div>
+	<Container>
+	  <StyledText>My Account</StyledText>
+	  <Button>로그아웃</Button>
+	</Container>
   );
 }
 
