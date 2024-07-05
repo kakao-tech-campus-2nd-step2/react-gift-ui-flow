c2142919a22ed779858d3d5c3ea9549e54d3cfcb
Dobbymin <gmin0701@knu.ac.kr>
2024-07-05 18:33:26 +0900

    feat(theme): uri 추출 후 props 넘겨주기 기능 구현

--- src/pages/theme/ThemePage.tsx
+++ src/pages/theme/ThemePage.tsx
@@ -1,10 +1,12 @@
+import { useParams } from 'react-router-dom';
 import GiftSection from './components/GiftSection';
 import TitleSection from './components/TitleSection';
 
 const ThemePage = () => {
+  const GiftPath = useParams();
   return (
     <>
-      <TitleSection />
+      <TitleSection pathData={GiftPath.themeKey as string} />
       <GiftSection />
     </>
   );
