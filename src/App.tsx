29c451912a7558e7fc5ec5442e19b3d53780d759
2021048695 <hanyanging@hanyang.ac.kr>
2024-07-05 22:17:33 +0900

    chore: App.tsx 파일 수정 & 기타 설정 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import React from 'react';
 
-const App = () => {
-  const name = 'Josh Perez';
+import Footer from './components/common/Footer/Footer';
+import Header from './components/common/Header/Header';
 
+const appStyle = css`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const mainContentStyle = css`
+  flex: 1;
+`;
+
+const App: React.FC = () => {
   return (
-    <div>
-      <Title>Hello, {name}</Title>
+    <div css={appStyle}>
+      <Header />
+      <main css={mainContentStyle} />
+      <Footer />
     </div>
   );
 };
 
 export default App;
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  color: gray;
-`;
