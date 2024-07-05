c9aea7e829ab41c15dc694e4c2bcf0ce9fabd862
yuni <hys990730@gmail.com>
2024-07-05 15:25:38 +0900

    feat: 금액 기준으로 아이템 정렬 기능 추가

--- src/pages/MainPage/ItemList.tsx
+++ src/pages/MainPage/ItemList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Button } from '@/components/common/Button';
 import { RankingGoodsItems } from '@/components/common/GoodsItem/Ranking';
@@ -16,9 +16,16 @@ interface ItemListProps {
 }
 
 const ItemList = ({ items }: ItemListProps) => {
-  const [filter1, setFilter1] = useState('모두가');
-  const [filter2, setFilter2] = useState('받고 싶어한');
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
   const [visibleItems, setVisibleItems] = useState(6);
+  const [filteredItems, setFilteredItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    const sortedItems = [...items].sort((a, b) =>
+      sortOrder === 'asc' ? a.amount - b.amount : b.amount - a.amount,
+    );
+    setFilteredItems(sortedItems);
+  }, [items, sortOrder]);
 
   const handleMore = () => {
     setVisibleItems((prevVisibleItems) => prevVisibleItems + 6);
@@ -32,20 +39,13 @@ const ItemList = ({ items }: ItemListProps) => {
     <section>
       <h2>실시간 급상승 선물랭킹</h2>
       <div>
-        <select value={filter1} onChange={(e) => setFilter1(e.target.value)}>
-          <option value="모두가">모두가</option>
-          <option value="여성이">여성이</option>
-          <option value="남성이">남성이</option>
-          <option value="청소년">청소년이</option>
-        </select>
-        <select value={filter2} onChange={(e) => setFilter2(e.target.value)}>
-          <option value="받고 싶어한">받고 싶어한</option>
-          <option value="많이 선물한">많이 선물한</option>
-          <option value="위시로 받은">위시로 받은</option>
+        <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value as 'asc' | 'desc')}>
+          <option value="asc">금액 낮은 순</option>
+          <option value="desc">금액 높은 순</option>
         </select>
       </div>
       <Grid columns={{ sm: 4, md: 5, lg: 6 }} gap={20}>
-        {items.slice(0, visibleItems).map((item, index) => (
+        {filteredItems.slice(0, visibleItems).map((item, index) => (
           <RankingGoodsItems
             key={index}
             rankingIndex={index + 1}
@@ -56,7 +56,7 @@ const ItemList = ({ items }: ItemListProps) => {
           />
         ))}
       </Grid>
-      {visibleItems < items.length ? (
+      {visibleItems < filteredItems.length ? (
         <Button onClick={handleMore} theme="outline" size="large">
           더보기
         </Button>
