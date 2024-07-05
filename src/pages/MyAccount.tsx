a9b0faca4627d471d91d1bda72f203ae8f3ad109
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2024-07-05 23:44:59 +0900

    feat : my account page

--- src/pages/MyAccount.tsx
+++ src/pages/MyAccount.tsx
@@ -1,7 +1,10 @@
+import '@styles/myAccount.css';
+
 function MyAccount() {
   return (
-    <div>
-      <h1>My Account</h1>
+    <div className="my-account">
+      <p className="my-account__title">닉네임님 안녕하세요!</p>
+      <button className="my-account__logout">로그아웃</button>
     </div>
   );
 }
