eb3bb73abd80924b1d8b33ebad1a077f4cd0f93b
Kuheewon <selly3109@naver.com>
2024-07-05 17:16:41 +0900

    feat. 해더 색션 추가

--- src/pages/ThemePage.tsx
+++ src/pages/ThemePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import ThemeHeader from '../components/common/Header/ThemeHeader';
+
 const ThemePage: React.FC = () => {
   const { themeKey } = useParams<{ themeKey: string }>();
 
   return (
     <div>
-      <h2>Theme Page</h2>
+      <ThemeHeader />
       <p>This is the theme page for: {themeKey}</p>
     </div>
   );
