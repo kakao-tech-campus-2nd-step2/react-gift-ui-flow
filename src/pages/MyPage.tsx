c66f75f2838841da2b5ba63032c78e5f1ffe00a8
seung365 <bdh3659@naver.com>
2024-07-05 15:14:30 +0900

    feat: MyPage 구현

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,5 +1,46 @@
+import { Button } from '@/components/common/Button';
+import { useNavigate } from 'react-router-dom';
+import { useUserInfo } from '@/providers/AuthProvider';
+import { BodyWrapper } from '@/pages/MainPage';
+import styled from '@emotion/styled';
+
 const MyPage = () => {
-  return <div>mypage 입니다.</div>;
+  const handleLogout = () => {
+    sessionStorage.removeItem('authToken');
+    handleUser('logout');
+  };
+
+  const navigate = useNavigate();
+
+  const { handleUser } = useUserInfo();
+
+  const handleLink = () => {
+    navigate('/');
+    handleLogout();
+  };
+
+  const name = sessionStorage.getItem('authToken');
+
+  return (
+    <MyPageWrapper>
+      <h1>{name}님 안녕하세요!</h1>
+      <MyPageButton onClick={handleLink} size="responsive">
+        로그아웃
+      </MyPageButton>
+    </MyPageWrapper>
+  );
 };
 
 export default MyPage;
+
+const MyPageWrapper = styled(BodyWrapper)`
+  height: 80vh;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+const MyPageButton = styled(Button)`
+  width: 50%;
+`;
