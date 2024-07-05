e79f5ad2aaf2152b3b400ee6e3ffad250ae2a464
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-07-05 15:41:57 +0900

    chore: Remove unnecessary files and folders

--- craco.config.js
+++ craco.config.js
@@ -5,13 +5,9 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
       '@components': path.resolve(__dirname, 'src/components'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@apis': path.resolve(__dirname, 'src/apis'),
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),
       '@pages': path.resolve(__dirname, 'src/pages'),
-      '@store': path.resolve(__dirname, 'src/store'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
       '@data': path.resolve(__dirname, 'src/data'),
       '@routes': path.resolve(__dirname, 'src/routes'),
       '@context': path.resolve(__dirname, 'src/context'),
