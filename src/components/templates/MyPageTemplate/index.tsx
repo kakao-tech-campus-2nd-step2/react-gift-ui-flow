ec041c82246e7dc05986ffefbe8fd90a5e26259d
JaeAnHan <jaean1999@naver.com>
2024-07-05 18:07:32 +0900

    feat: 마이페이지 로그인 안됐을 때 주소창으로 들어오면 로그인 페이지로, 아니면 인덱스 페이지로 리다이렉트

--- src/components/templates/MyPageTemplate/index.tsx
+++ src/components/templates/MyPageTemplate/index.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import type { Dispatch, SetStateAction } from 'react';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Button } from '@/components/atoms/Button';
@@ -9,22 +9,32 @@ import { ContainerDirectionType } from '@/components/atoms/FlatFlex/types';
 import { Paragraph } from '@/components/atoms/Paragraph';
 import type { Auth } from '@/components/contexts/AuthContext';
 import { AuthContext, defaultAuthValue, SetAuthContext } from '@/components/contexts/AuthContext';
+import { RouterPath } from '@/router';
 
 export const MyPageTemplate = () => {
+  const isFirstJumpedIn = useRef(true);
   const navigate = useNavigate();
   const auth = useContext(AuthContext);
   const setAuth = useContext(SetAuthContext) as Dispatch<SetStateAction<Auth>>;
   const onClick = () => {
+    isFirstJumpedIn.current = false;
     sessionStorage.setItem('authToken', JSON.stringify(defaultAuthValue));
     setAuth(defaultAuthValue);
-    navigate('/');
   };
 
   useEffect(() => {
     if (auth.name === undefined) {
-      navigate('login');
+      /**
+       * 주소창 localhost:3000/my-account으로 바로 들어오면 login으로 보내고
+       * 마이페이지에서 로그아웃하면 /로 보내기
+       */
+      if (isFirstJumpedIn.current) {
+        navigate(RouterPath.login);
+      } else {
+        navigate(RouterPath.root);
+      }
     }
-  }, [auth.name, navigate]);
+  });
 
   return (
     <FullHeightFlex flexDirection={ContainerDirectionType.column}>
