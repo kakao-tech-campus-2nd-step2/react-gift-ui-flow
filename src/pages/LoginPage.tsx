a09ca7f5f5387023b06293cac1165881ef02d317
rudtj <kyoungseo03@naver.com>
2024-07-05 15:45:59 +0900

    refactor: 컴포넌트 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,73 +1,30 @@
 import styled from '@emotion/styled';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
-import { Button } from '../components/common/Button';
-import { UnderlineTextField } from '../components/common/Form/Input/UnderlineTextField';
+import { AuthProvider } from '../components/common/Login/AuthContext';
+import LoginForm from '../components/common/Login/LoginForm';
 
 const LoginPageContainer = styled.section`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
 `;
 
-const LoginForm = styled.form`
-  width: 100%;
-  max-width: 580px;
-  padding: 16px;
-  & > h3 {
-    font-size: 30px;
-    padding-bottom: 40px;
-    text-align: center;
-  }
-`;
-
-const Box = styled.section`
-  box-sizing: border-box;
-  border: 1px solid #ccc;
-  padding: 50px;
-`;
-
-const ButtonContainer = styled.section`
-  padding-top: 40px;
+const LoginTitle = styled.img`
+  width: 88px;
 `;
 
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const navigate = useNavigate();
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    navigate('/');
-  };
-
   return (
     <LoginPageContainer>
-      <LoginForm onSubmit={handleSubmit}>
-        <h3>KaKao</h3>
-        <Box>
-          <UnderlineTextField
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="이름"
-          />
-          <UnderlineTextField
-            type="text"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="비밀번호"
-          />
-          <ButtonContainer>
-            <Button type="submit">로그인</Button>
-          </ButtonContainer>
-        </Box>
-      </LoginForm>
+      <LoginTitle
+        src="https://kakao-tech-week2-kakao-gift-ui-flow.pages.dev/static/media/kakao_logo.60e99555125ae4c4b8f23f6c5faee9ec.svg"
+        alt="kakao"
+      />
+      <AuthProvider>
+        <LoginForm />
+      </AuthProvider>
     </LoginPageContainer>
   );
 }
