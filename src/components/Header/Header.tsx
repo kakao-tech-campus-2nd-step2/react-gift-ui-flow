3b7cd0827be69b888045eac96d9431c2f29c7b5c
seung365 <bdh3659@naver.com>
2024-07-05 15:13:32 +0900

    feat: header에서  로그인과 내 계정 연결

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,38 @@
 import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 import { Image } from '../common/Image';
+import { useUserInfo } from '@/providers/AuthProvider';
+
+const Header = () => {
+  const navigate = useNavigate();
+  const { state } = useUserInfo();
+
+  const handleLink = (path: string) => {
+    navigate(path);
+  };
+
+  const handleWhere = (where: string) => {
+    if (where === 'login') handleLink('/my-account');
+    else handleLink('/login');
+  };
+
+  return (
+    <HeaderWrapper>
+      <Image
+        style={{ cursor: 'pointer' }}
+        src="/images/gift_logo.png"
+        onClick={() => handleLink('/')}
+        width={60}
+      ></Image>
+      <h3 style={{ cursor: 'pointer' }} onClick={() => handleWhere(state)}>
+        {state === 'login' ? '내 계정' : '로그인'}
+      </h3>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
+
 const HeaderWrapper = styled.header`
   position: fixed;
   top: 0;
@@ -14,25 +46,3 @@ const HeaderWrapper = styled.header`
   background-color: #fff;
   z-index: 3;
 `;
-
-const Header = () => {
-  const navigate = useNavigate();
-  const handleLink = (path: string) => {
-    navigate(path);
-  };
-  return (
-    <HeaderWrapper>
-      <Image
-        style={{ cursor: 'pointer' }}
-        src="/images/gift_logo.png"
-        onClick={() => handleLink('/')}
-        width={60}
-      ></Image>
-      <h3 style={{ cursor: 'pointer' }} onClick={() => handleLink('/login')}>
-        로그인
-      </h3>
-    </HeaderWrapper>
-  );
-};
-
-export default Header;
