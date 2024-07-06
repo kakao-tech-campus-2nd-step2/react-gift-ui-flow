3f24a6a43da7abae79c1fa2a766c27c3ef7f07bd
yunseong0404 <333665@naver.com>
2024-07-06 12:16:53 +0900

    fix: Main 페이지 분리

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import '@/App.css';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
 // import React from 'react';
-import GiftRanking from '@/components/common/main/GiftRanking';
-import ThemeCategory from '@/components/common/main/ThemeCategory';
 import { IsLoginProvider } from '@/components/contextAPI/IsLoginContext';
 import Layout from '@/components/Layout/Layout';
 import Login from '@/pages/login/Login';
+import Main from '@/pages/main/Main';
 import MyPage from '@/pages/myPage/MyPage';
 import Theme from '@/pages/theme/Theme';
 
@@ -18,7 +17,7 @@ const App = () => {
       <Router>
         <Layout>
           <Routes>
-            <Route path='/' element={<Home />}/>
+            <Route path='/' element={<Main />}/>
             <Route path='/theme/:themeKey' element={<Theme />}/>
             <Route path='/login' element={<Login />}/>
             <Route path='/my-account' element={<MyPage />}/>
@@ -29,14 +28,6 @@ const App = () => {
   );
 };
 
-function Home() {
-  return(
-    <div>
-      <ThemeCategory/>
-      <GiftRanking/>
-    </div>
-  );
-};
 
 export default App;
 
