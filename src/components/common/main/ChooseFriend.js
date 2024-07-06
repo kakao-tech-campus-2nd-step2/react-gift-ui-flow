674545cf2675b8ab86d563f846918431c4025156
yunseong0404 <333665@naver.com>
2024-07-06 12:12:41 +0900

    fix: ChooseFriend.js handleClick 함수 추가

--- src/components/common/main/ChooseFriend.js
+++ src/components/common/main/ChooseFriend.js
@@ -6,6 +6,10 @@ import { Image } from '@/components/common/Image/index.tsx';
 
 
 export default function ChooseFriend() {
+  const handleClick = () => {
+    alert("선물 받을 친구 선택하기");
+  }
+
   return (
     <div className='ChoseFriend-container'>
         <div className='ChoseFriend-inner'>
@@ -15,6 +19,7 @@ export default function ChooseFriend() {
                 width='70px'
                 ratio='square'
                 radius={25}
+                onClick={handleClick}
             />
             <p className='ChoseFriend-inner__text'>선물 받을 친구를 선택해주세요</p>
         </div>
