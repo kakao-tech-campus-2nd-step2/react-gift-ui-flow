85d45576543aa9fec3c9ffc3e5a92423a4df9952
yunseong0404 <333665@naver.com>
2024-07-06 13:33:49 +0900

    fix: themeData 분리

---
+++ src/components/common/Theme/themeData.js
@@ -0,0 +1,110 @@
+export const themeData = [
+  {
+    key: 'birthday',
+    data: {
+      label: '생일',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+  {
+    key: 'graduation_gift',
+    data: {
+      label: '졸업 선물',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+  {
+    key: 'small_luxury',
+    data: {
+      label: '스몰 럭셔리',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+  {
+    key: 'luxury_gift',
+    data: {
+      label: '명품 선물',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+  {
+    key: 'marriage-households',
+    data: {
+      label: '결혼/집들이',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+  {
+    key: 'warm_present',
+    data: {
+      label: '따뜻한 선물',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+  {
+    key: 'light_present',
+    data: {
+      label: '가벼운 선물',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+  {
+    key: 'for_the_fans',
+    data: {
+      label: '팬심 저격',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+  {
+    key: 'coupon',
+    data: {
+      label: '교환권',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+  {
+    key: 'health-vitamin',
+    data: {
+      label: '건강/비타민',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+  {
+    key: 'fruit-korean_beef',
+    data: {
+      label: '과일/한우',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+  {
+    key: 'birth-kids',
+    data: {
+      label: '출산/키즈',
+      title: '예산은 가볍게, 감동은 무겁게❤️',
+      description: '당신의 센스를 뽐내줄 부담 없는 선물',
+      bgColor: '#fff',
+    },
+  },
+];
