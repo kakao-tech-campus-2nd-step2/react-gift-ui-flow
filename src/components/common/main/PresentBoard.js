9e3dca777f4310f04a931c6f25746bee42bbc8c6
yunseong0404 <333665@naver.com>
2024-07-06 13:34:13 +0900

    fix: PresentBoard.js themeData 추가

--- src/components/common/main/PresentBoard.js
+++ src/components/common/main/PresentBoard.js
@@ -2,24 +2,17 @@ import './PresentBoard.css';
 
 import React from 'react';
 
+import { themeData } from '@/components/common/Theme/themeData';
+
 import PresentItem from './PresentItem';
 
 export default function PresentBoard() {
   return (
     <div className="PresentBoard-container">
       <div className="PresentBoard">
-        <PresentItem themeKey="birthday" giftCategory="생일" />
-        <PresentItem themeKey="graduation_gift" giftCategory="졸업선물" />
-        <PresentItem themeKey="small_luxury" giftCategory="스몰럭셔리" />
-        <PresentItem themeKey="luxury_gift" giftCategory="명품선물" />
-        <PresentItem themeKey="marriage-households" giftCategory="결혼/집들이" />
-        <PresentItem themeKey="warm_present" giftCategory="따뜻한선물" />
-        <PresentItem themeKey="light_present" giftCategory="가벼운선물" />
-        <PresentItem themeKey="for_the_fans" giftCategory="팬심저격" />
-        <PresentItem themeKey="coupon" giftCategory="교환권" />
-        <PresentItem themeKey="health-vitamin" giftCategory="건강/비타민" />
-        <PresentItem themeKey="fruit-korean_beef" giftCategory="과일/한우" />
-        <PresentItem themeKey="birth-kids" giftCategory="출산/키즈" />
+        {themeData.map((theme) => (
+            <PresentItem themeKey={theme.key} giftCategory={theme.data.label} />
+        ))}
       </div>
     </div>
   );
