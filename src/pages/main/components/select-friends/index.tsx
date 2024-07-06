6790535921150fa6e1ab53ead868721bff9f7e99
Dobbymin <gmin0701@knu.ac.kr>
2024-07-06 18:19:54 +0900

    feat(main): 선물 받을 친구 선택 alert 창 추가

--- src/pages/main/components/select-friends/index.tsx
+++ src/pages/main/components/select-friends/index.tsx
@@ -4,11 +4,15 @@ import * as G from '@/styles/globalStyles';
 import SelectImg from '@/assets/main/Friends_Icon.png';
 
 const SelectFriend = () => {
+  const handleSelect = () => {
+    alert('선물 받을 친구 선택하기');
+  };
+
   return (
     <S.SelectFriendsLayout>
       <Container>
         <G.InnerContainer justifyContent="flex-start">
-          <S.FriendImg src={SelectImg} alt="친구 선택 유도 아이콘" />
+          <S.FriendImg onClick={handleSelect} src={SelectImg} alt="친구 선택 유도 아이콘" />
           <S.Text>선물 받을 친구를 선택해주세요.</S.Text>
         </G.InnerContainer>
       </Container>
