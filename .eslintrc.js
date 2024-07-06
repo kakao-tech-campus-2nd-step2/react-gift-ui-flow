3008c0af2d7866231b9abf9a58d2e96170cbe92a
Dobbymin <gmin0701@knu.ac.kr>
2024-07-06 17:12:58 +0900

    feat(eslint): import 대소문자 구별 규칙 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,16 @@ module.exports = {
     'prettier',
   ],
   rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+        alphabetize: {
+          order: 'desc',
+          caseInsensitive: true,
+        },
+      },
+    ],
     'react/react-in-jsx-scope': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
