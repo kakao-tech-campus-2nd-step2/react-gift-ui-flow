74768a889561a611b16082346019f238a9361960
yunseong0404 <333665@naver.com>
2024-07-06 12:10:03 +0900

    fix: App.tsx Login, MyPage 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,35 @@ import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 // import React from 'react';
 import GiftRanking from '@/components/common/main/GiftRanking';
 import ThemeCategory from '@/components/common/main/ThemeCategory';
-import Footer from '@/components/Footer/Footer';
-import Header from '@/components/Header/Header';
-import Theme from '@/components/pages/theme/Theme.js';
-
+import { IsLoginProvider } from '@/components/contextAPI/IsLoginContext';
+import Layout from '@/components/Layout/Layout';
+import Login from '@/pages/login/Login';
+import MyPage from '@/pages/myPage/MyPage';
+import Theme from '@/pages/theme/Theme';
 
 const App = () => {
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/theme/:themeKey' element={<Theme />}/>
-      </Routes>
-    </Router>
+    <IsLoginProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/theme/:themeKey' element={<Theme />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/my-account' element={<MyPage />}/>
+          </Routes>
+        </Layout>
+      </Router>
+    </IsLoginProvider>
   );
 };
 
 function Home() {
   return(
     <div>
-      <Header/>
       <ThemeCategory/>
       <GiftRanking/>
-      <Footer/>
     </div>
   );
 };
