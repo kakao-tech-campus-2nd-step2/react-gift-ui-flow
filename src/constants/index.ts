ad8dcb5617ae7b22aa5889670807b275e5648401
cla6shade <cla6shade@gmail.com>
2024-07-07 16:02:47 +0900

    fix: 상수가 정의되어 있지 않던 문제점 해결
    branch를 병합하다가 나온 실수..

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
-const TEST_IMAGE_URL = 'https://raw.githubusercontent.com/cla6shade/Cla6Shade/master/banner.png';
+export const TEST_IMAGE_URL = 'https://raw.githubusercontent.com/cla6shade/Cla6Shade/master/banner.png';
 
-// eslint-disable-next-line import/prefer-default-export
-export { TEST_IMAGE_URL };
+export const STORAGE_AUTH_TOKEN_KEY = 'AUTH_TOKEN';
