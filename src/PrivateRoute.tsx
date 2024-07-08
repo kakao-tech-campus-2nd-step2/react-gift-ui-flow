1448a49c61f8e358ec83dac1050bf7cbeda02e14
Kuheewon <selly3109@naver.com>
2024-07-08 16:16:00 +0900

    refactor. 코드 리팩토링

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { useAuth } from './context/AuthContext';
+import { ROUTES } from './routes';
 
 const PrivateRoute: React.FC<{ element: React.ReactElement }> = ({ element }) => {
   const { isLoggedIn } = useAuth();
 
   if (!isLoggedIn) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to={ROUTES.LOGIN} replace />;
   }
 
   return element;
