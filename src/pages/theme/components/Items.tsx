664624d1ccce606966d3b49e54a111ac99e4b0dd
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-07-08 15:47:13 +0900

    refactor: render base on the data

--- src/pages/theme/components/Items.tsx
+++ src/pages/theme/components/Items.tsx
@@ -3,88 +3,86 @@ import { Grid } from '@/components/common/layouts/Grid';
 
 import styles from './Items.module.scss';
 
+const itemsData = [
+  {
+    imageSrc:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+    subtitle: 'BBQ',
+    title: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    amount: 29000,
+  },
+  {
+    imageSrc:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+    subtitle: 'BBQ',
+    title: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    amount: 29000,
+  },
+  {
+    imageSrc:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+    subtitle: 'BBQ',
+    title: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    amount: 29000,
+  },
+  {
+    imageSrc:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+    subtitle: 'BBQ',
+    title: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    amount: 29000,
+  },
+  {
+    imageSrc:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+    subtitle: 'BBQ',
+    title: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    amount: 29000,
+  },
+  {
+    imageSrc:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+    subtitle: 'BBQ',
+    title: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    amount: 29000,
+  },
+  {
+    imageSrc:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+    subtitle: 'BBQ',
+    title: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    amount: 29000,
+  },
+  {
+    imageSrc:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+    subtitle: 'BBQ',
+    title: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    amount: 29000,
+  },
+  {
+    imageSrc:
+      'https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg',
+    subtitle: 'BBQ',
+    title: 'BBQ 양념치킨+크림치즈볼+콜라1.25L',
+    amount: 29000,
+  },
+  // 필요한 만큼 아이템 추가
+];
+
 const Items = () => {
   return (
     <div className={styles.items}>
       <Grid columns={2} gap={15}>
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
-        <DefaultGoodsItems
-          imageSrc="https://st.kakaocdn.net/product/gift/product/20231030175450_53e90ee9708f45ffa45b3f7b4bc01c7c.jpg"
-          subtitle="BBQ"
-          title="BBQ 양념치킨+크림치즈볼+콜라1.25L"
-          amount={29000}
-        />
+        {itemsData.map((item, index) => (
+          <DefaultGoodsItems
+            key={index}
+            imageSrc={item.imageSrc}
+            subtitle={item.subtitle}
+            title={item.title}
+            amount={item.amount}
+          />
+        ))}
       </Grid>
     </div>
   );
