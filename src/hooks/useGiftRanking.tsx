8421513ad30fecafbd445ed45188331ae293eea7
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-07-08 14:44:20 +0900

    feat : useGiftRanking.tsx 파일에 Filter, SubFilter 타입 정의 추가

--- src/hooks/useGiftRanking.tsx
+++ src/hooks/useGiftRanking.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 
-import type { GiftItem} from '../data/giftItems';
-import {giftItems } from '../data/giftItems';
+import type { GiftItem } from '../data/giftItems';
+import { giftItems } from '../data/giftItems';
+
+type Filter = '전체' | '여성이' | '남성이' | '청소년이';
+type SubFilter = '받고 싶어한' | '많이 선물한' | '위시로 받은';
 
 const useGiftRanking = () => {
-  const [filter, setFilter] = useState<string>('전체');
-  const [subFilter, setSubFilter] = useState<string>('받고 싶어한');
+  const [filter, setFilter] = useState<Filter>('전체');
+  const [subFilter, setSubFilter] = useState<SubFilter>('받고 싶어한');
   const [showMore, setShowMore] = useState<boolean>(false);
 
   const items: GiftItem[] = giftItems;
