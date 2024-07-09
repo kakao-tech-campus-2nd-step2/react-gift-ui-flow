bd35854a753dd007909396ac246a49bb815cb0a7
wjdsbs <wjdsbss@gmail.com>
2024-07-10 01:07:09 +0900

    refactor: 코드 중복 제거

--- src/components/MainPage/ThemeGrid.tsx
+++ src/components/MainPage/ThemeGrid.tsx
@@ -21,163 +21,108 @@ const Wrapper = styled(Grid)`
   max-width: 100%;
 `;
 
+interface ThemeItemComponentProps {
+  link: string;
+  src: string;
+  alt: string;
+  label: string;
+}
+
+const ThemeItemComponent = ({ link, src, alt, label }: ThemeItemComponentProps) => (
+  <Link to={link}>
+    <ThemeItem>
+      <ThemeImage src={src} alt={alt} width="90px" radius={32} ratio="square" />
+      <p>{label}</p>
+    </ThemeItem>
+  </Link>
+);
+
+const themeData: ThemeItemComponentProps[] = [
+  {
+    link: '/theme/life_small_gift',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '생일',
+    label: '생일',
+  },
+  {
+    link: '/theme/:themekey',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '졸업선물',
+    label: '졸업선물',
+  },
+  {
+    link: '/theme/:themekey',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '스몰럭셔리',
+    label: '스몰럭셔리',
+  },
+  {
+    link: '/theme/:themekey',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '명품선물',
+    label: '명품선물',
+  },
+  {
+    link: '/theme/:themekey',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '결혼/집들이',
+    label: '결혼/집들이',
+  },
+  {
+    link: '/theme/:themekey',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '따뜻한선물',
+    label: '따뜻한선물',
+  },
+  {
+    link: '/theme/:themekey',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '가벼운선물',
+    label: '가벼운선물',
+  },
+  {
+    link: '/theme/:themekey',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '팬심저격',
+    label: '팬심저격',
+  },
+  {
+    link: '/theme/:themekey',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '교환권',
+    label: '교환권',
+  },
+  {
+    link: '/theme/:themekey',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '건강/비타민',
+    label: '건강/비타민',
+  },
+  {
+    link: '/theme/:themekey',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '과일/한우',
+    label: '과일/한우',
+  },
+  {
+    link: '/theme/:themekey',
+    src: 'https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png',
+    alt: '출산/키즈',
+    label: '출산/키즈',
+  },
+];
+
 const ThemeGrid = () => (
   <Wrapper columns={6} gap={12}>
-    <Link to="/theme/life_small_gift">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="생일"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>생일</p>
-      </ThemeItem>
-    </Link>
-
-    <Link to="/theme/:themekey">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="졸업선물"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>졸업선물</p>
-      </ThemeItem>
-    </Link>
-
-    <Link to="/theme/:themekey">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="스몰럭셔리"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>스몰럭셔리</p>
-      </ThemeItem>
-    </Link>
-
-    <Link to="/theme/:themekey">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="명품선물"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>명품선물</p>
-      </ThemeItem>
-    </Link>
-
-    <Link to="/theme/:themekey">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="결혼/집들이"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>결혼/집들이</p>
-      </ThemeItem>
-    </Link>
-
-    <Link to="/theme/:themekey">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="따뜻한선물"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>따뜻한선물</p>
-      </ThemeItem>
-    </Link>
-
-    <Link to="/theme/:themekey">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="가벼운선물"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>가벼운선물</p>
-      </ThemeItem>
-    </Link>
-
-    <Link to="/theme/:themekey">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="팬심저격"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>팬심저격</p>
-      </ThemeItem>
-    </Link>
-
-    <Link to="/theme/:themekey">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="교환권"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>교환권</p>
-      </ThemeItem>
-    </Link>
-
-    <Link to="/theme/:themekey">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="건강/비타민"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>건강/비타민</p>
-      </ThemeItem>
-    </Link>
-
-    <Link to="/theme/:themekey">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="과일/한우"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>과일/한우</p>
-      </ThemeItem>
-    </Link>
-
-    <Link to="/theme/:themekey">
-      <ThemeItem>
-        <ThemeImage
-          src="https://img1.daumcdn.net/thumb/S104x104/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fgift%2Fhome%2Ftheme%2F292020231106_MXMUB.png"
-          alt="출산/키즈"
-          width="90px"
-          radius={32}
-          ratio="square"
-        />
-        <p>출산/키즈</p>
-      </ThemeItem>
-    </Link>
+    {themeData.map((theme, index) => (
+      <ThemeItemComponent
+        key={index}
+        link={theme.link}
+        src={theme.src}
+        alt={theme.alt}
+        label={theme.label}
+      />
+    ))}
   </Wrapper>
 );
 
